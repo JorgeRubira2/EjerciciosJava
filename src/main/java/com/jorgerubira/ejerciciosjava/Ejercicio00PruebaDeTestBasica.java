0d861b47f4aa4d045cd0f0dc9eb15cadc3a9b70b
Jochemali <gimenezleon98@gmail.com>
2021-07-21 10:43:24 +0200

    Primera prueba

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java
@@ -4,14 +4,14 @@ package com.jorgerubira.ejerciciosjava;
 public class Ejercicio00PruebaDeTestBasica {
     
     public int sumar(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+       return a+b;
     }
 
     public int restar(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+       return a-b;
     }
 
     public int multiplicar(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+        return a*b;
     }
 }
