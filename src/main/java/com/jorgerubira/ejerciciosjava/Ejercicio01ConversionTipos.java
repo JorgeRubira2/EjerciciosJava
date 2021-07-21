f36d273289b91d83e8244d427cc917f501297c70
Sofia Rebagliato <sofia.rebagliatto@gmail.com>
2021-07-21 23:08:01 +0200

    Conversion de tipos

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTipos.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTipos.java
@@ -3,38 +3,43 @@ package com.jorgerubira.ejerciciosjava;
 public class Ejercicio01ConversionTipos {
 
     //Devuelve la suma a+b;
-    public int ejemploSuma(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+    public int ejemploSuma(int a, int b) {
+        return a + b;
     }
-    
+
     //Convierte de String a int.
     public int textoAEntero(String valor) {
-        throw new RuntimeException("Pendiente de hacer");
+        return Integer.parseInt(valor);
     }
 
     //Convierte de Float a int.
-    public int decimalesAEntero(Float valor){
-        throw new RuntimeException("Pendiente de hacer");
+    public int decimalesAEntero(Float valor) {
+        return valor.intValue();
     }
 
     //Convierte de int a Float.
-    public Float enteroAFloat(int valor){
-        throw new RuntimeException("Pendiente de hacer");
+    public Float enteroAFloat(int valor) {
+        return (float) valor;
     }
 
     //Devuelve el siguiente carácter en el alfabeto (tabla ASCII).
-    public char siguienteCaracter(char valor){
-        throw new RuntimeException("Pendiente de hacer");
+    public char siguienteCaracter(char valor) {
+        return String.valueOf(valor + 1).charAt(0);
     }
 
-    //Devuelve el código ascii del caracter 
-    public int obtenerAscii(char valor){
-        throw new RuntimeException("Pendiente de hacer");
+    //Devuelve el código ascii del caracter. 
+    public int obtenerAscii(char valor) {
+        return (int) valor;
     }
 
     //Convierte de String a Double. Devuelve null si no se puede convertir
-    public Double textoADouble(String valor){
-        throw new RuntimeException("Pendiente de hacer");
+    public Double textoADouble(String valor) {
+
+        try {
+            return Double.parseDouble(valor);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
