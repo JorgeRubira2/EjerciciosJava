336feb1be3888da323a62cc7a361fa5f8993aaf7
Javier A <javialiaga2000@gmail.com>
2021-07-22 08:53:30 +0200

    Primera Prueba

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTipos.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTipos.java
@@ -4,37 +4,41 @@ public class Ejercicio01ConversionTipos {
 
     //Devuelve la suma a+b;
     public int ejemploSuma(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+        return a + b;
     }
     
     //Convierte de String a int.
     public int textoAEntero(String valor) {
-        throw new RuntimeException("Pendiente de hacer");
+        return Integer.parseInt(valor);
     }
 
     //Convierte de Float a int.
     public int decimalesAEntero(Float valor){
-        throw new RuntimeException("Pendiente de hacer");
+        return (int)valor.intValue();
     }
 
     //Convierte de int a Float.
     public Float enteroAFloat(int valor){
-        throw new RuntimeException("Pendiente de hacer");
+        return (float)valor;
     }
 
     //Devuelve el siguiente carácter en el alfabeto (tabla ASCII).
     public char siguienteCaracter(char valor){
-        throw new RuntimeException("Pendiente de hacer");
+        return (char)(valor+1);
     }
 
     //Devuelve el código ascii del caracter 
     public int obtenerAscii(char valor){
-        throw new RuntimeException("Pendiente de hacer");
+        return (int)valor;
     }
 
     //Convierte de String a Double. Devuelve null si no se puede convertir
     public Double textoADouble(String valor){
-        throw new RuntimeException("Pendiente de hacer");
+        try{
+            return Double.parseDouble(valor);
+        }catch(Exception e){
+            return null;
+        }
     }
 
 }
