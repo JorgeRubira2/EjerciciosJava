21dad5c6aefacb04078cadef42211874e1bde90c
JorgeRubira <jorgesub@gmail.com>
2021-07-22 08:01:21 +0200

    Primera prueba

--- src/test/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTiposTest.java
+++ src/test/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTiposTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jorgerubira.ejerciciosjava;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,7 +18,7 @@ public class Ejercicio01ConversionTiposTest {
     @org.junit.jupiter.api.Test
     public void testDecimalesAEntero() {
         Ejercicio01ConversionTipos instance = new Ejercicio01ConversionTipos();
-        assertEquals(24, instance.decimalesAEntero(-24.2f));
+        assertEquals(-24, instance.decimalesAEntero(-24.2f));
         assertEquals(44, instance.decimalesAEntero(44.8f));
     }
 
@@ -31,7 +26,7 @@ public class Ejercicio01ConversionTiposTest {
     public void testEnteroAFloat() {
         Ejercicio01ConversionTipos instance = new Ejercicio01ConversionTipos();
         assertEquals(2f, instance.enteroAFloat(2));
-        assertEquals(-2f, instance.enteroAFloat(-5));
+        assertEquals(-5f, instance.enteroAFloat(-5));
     }
 
     @org.junit.jupiter.api.Test
