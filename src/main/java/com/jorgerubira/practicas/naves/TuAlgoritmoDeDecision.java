d5bbe22eebfee78dbde165140ce6bd096ee2e670
Javier A <javialiaga2000@gmail.com>
2021-07-23 14:56:41 +0200

    cohete

--- src/main/java/com/jorgerubira/practicas/naves/TuAlgoritmoDeDecision.java
+++ src/main/java/com/jorgerubira/practicas/naves/TuAlgoritmoDeDecision.java
@@ -13,7 +13,35 @@ public class TuAlgoritmoDeDecision implements IDecision {
 
     @Override
     public void decision(INave n, int segundos) {
-        throw new UnsupportedOperationException("Pendiente de hacer."); 
-    }
-    
+        //Velocidad < 200
+        //Angulo [-20, 20]
+        
+        if(n.getVelocidad() == 0 && n.getMetrosRecorridos() == 0){
+            n.setPropulsion(true);
+        }
+        if(n.getAngulo() == 0 && n.getMetrosRecorridos() == 0){
+            n.girarMando(20);
+        }
+        if(segundos == 4){
+            n.setPropulsion(false);
+            n.girarMando(-13);
+        }
+        
+        if(segundos >= 11 && segundos <= 12){
+            n.setPropulsion(true);
+            n.girarMando(2);
+            
+        }/*
+        if(segundos >= 12 && segundos <= 13){
+            n.setPropulsion(false);
+        }
+        if(segundos >= 13 && segundos <= 14){
+             n.girarMando(5);
+            n.setPropulsion(true);
+        }*/
+        if(segundos >= 13 ){
+            n.setPropulsion(false);
+        }
+     
+    }   
 }
