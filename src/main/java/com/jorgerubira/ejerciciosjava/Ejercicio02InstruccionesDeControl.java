cc0573ec859b8be18a31baeea0f48f406eccf2c8
Jesús <Jess@DESKTOP-VL21FGV>
2021-07-23 12:03:24 +0100

    Ejercicio 2 y 3

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio02InstruccionesDeControl.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio02InstruccionesDeControl.java
@@ -13,30 +13,54 @@ public class Ejercicio02InstruccionesDeControl {
     
     //Devolver el número más alto
     public int maximoValor(int a, int b, int c){
-        throw new RuntimeException("Pendiente de hacer");
+       if(a>=b && a>=c)return a;
+       else if(b>=a && b>=c) return b;
+       else return c;
     }
     
     //Devolver la suma de todos los elementos del vector
     public int sumarElementos(int[] vector){
-        throw new RuntimeException("Pendiente de hacer");
+        int result=0;
+        for(int elemento:vector){
+            result+=elemento;
+        }
+        return result;
     }    
     
     //Devolver cuantos elementos son pares
     public int contarPares(int[] vector){
-        throw new RuntimeException("Pendiente de hacer");
+       int pares=0;
+       for(int i=0; i<vector.length; i++){
+           if(vector[i]%2==0)pares++;
+       }
+       return pares;
     }    
     
     //Devolver el maximo comun divisor.
     //Recibir dos elementos a, b. Restar repetidamente el valor mas pequeño al más grande. Parar cuando son iguales.
     public int maximoComunDivisor(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+        while(a!=b){
+            if(a>b)
+                a-=b;
+            else b-=a;
+        }
+        return a;
     }
 
     //Contar vocales. Recibe una cadena y cuenta cuantas vocales hay.
     //1. Transformar el String y convertirlo a minúsculas toLowerCase(). 
     //2. Después recorrer los elementos hasta el final y obtener los caracteres con charAt(n). 
     public int contarVocales(String texto){
-        throw new RuntimeException("Pendiente de hacer");
+        String a=texto.toLowerCase();
+        int contar=0;
+        char []vocales={'a','e','i','o','u'};
+        for(int i=0;i<a.length(); i++){
+            for(int j=0;j<vocales.length;j++){
+                if(a.charAt(i)==vocales[j])contar++;
+            }
+        }
+        return contar;
+        
     }
     
 }
--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
@@ -8,35 +8,46 @@ public class Ejercicio03Comparadores {
     //Si no devuelve false
     //Si se envia null en cualquiera de ellos devuelve false
     public boolean compararEnteros(Integer i1, Integer i2){
-        throw new RuntimeException("Pendiente de hacer");
+        if(i1==null || i2==null)return false;
+        else if(i1.equals(i2))return true;
+        else return false;
     }
     
     //Debe devolver true si los dos enteros contienen el mismo valor.
     //Si no devuelve false
     //Si se envia null en cualquiera de ellos devuelve false
     public boolean compararEnteroConLong(Integer i1, Long i2){
-        throw new RuntimeException("Pendiente de hacer");
+        if(i1==null || i2==null)return false;
+        else if((long)i1==i2)return true;
+        else return false;
     }    
 
     //Debe devolver true si el texto que se ha enviado en un número. 
     //Si no devuelve false
     //Si se envia null devuelve false
     public boolean comprobarNumero(String numero){
-        throw new RuntimeException("Pendiente de hacer");
+        try{
+            Double.parseDouble(numero);
+            return true;
+        }catch(Exception e){return false;}
     }    
     
     //Debe devolver true si los dos números son iguales. 
     //Si no devuelve false
     //Si se envia null en cualquier de ellos devuelve false
     public boolean comprobarOptionalConInteger(Optional<Integer> valor1, Integer valor2){
-        throw new RuntimeException("Pendiente de hacer");
+      if(valor1.isEmpty() || valor2==null)return false;
+        else if(valor1.get().equals(valor2))return true;
+        else return false;
     }    
 
     //Debe devolver true si los dos números son iguales. 
     //Si no devuelve false
     //Si los dos tienen null devuelve false.
     public boolean comprobarOptionalesIntegerYDouble(Optional<Integer> valor1, Optional<Double> valor2){
-        throw new RuntimeException("Pendiente de hacer");
+        if(valor1.isEmpty() || valor2.isEmpty())return false;
+        else if(valor2.get().equals(Double.valueOf(valor1.get()))) return true;
+        else return false;
     }    
 
 }
