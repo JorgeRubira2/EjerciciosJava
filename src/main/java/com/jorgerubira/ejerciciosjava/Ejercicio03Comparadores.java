439e35372c9e303fd199ef37c5f947ccee5f03a9
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-07-23 11:56:35 +0200

    ejercicio3

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
@@ -8,27 +8,48 @@ public class Ejercicio03Comparadores {
     //Si no devuelve false
     //Si se envia null en cualquiera de ellos devuelve false
     public boolean compararEnteros(Integer i1, Integer i2){
-    	throw new RuntimeException("Pendiente de hacer");
+    	 
+    	if(i1 != null && i2 != null && (i1.equals(i2))) {
+    		return true;
+    	}else{
+    		return false;		
+    	}
+		
     }
     //Debe devolver true si los dos enteros contienen el mismo valor.
     //Si no devuelve false
     //Si se envia null en cualquiera de ellos devuelve false
     public boolean compararEnteroConLong(Integer i1, Long i2){
-        throw new RuntimeException("Pendiente de hacer");
+    	if(i1 != null && (Long)i2 != null && (i1.intValue()==i2.longValue())) {
+    		return true;
+    	}else{
+    		return false;		
+    	}
     }    
 
     //Debe devolver true si el texto que se ha enviado en un número. 
     //Si no devuelve false
     //Si se envia null devuelve false
     public boolean comprobarNumero(String numero){
-        throw new RuntimeException("Pendiente de hacer");
+    	
+    	try {
+			Double.parseDouble(numero);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
     }    
     
     //Debe devolver true si los dos números son iguales. 
     //Si no devuelve false
     //Si se envia null en cualquier de ellos devuelve false
     public boolean comprobarOptionalConInteger(Optional<Integer> valor1, Integer valor2){
-        throw new RuntimeException("Pendiente de hacer");
+    	if(valor1 != null && valor2 != null && (valor1.equals(valor2))) {
+    		return true;
+    	}else{
+    		return false;		
+    	}
+        
     }    
 
     //Debe devolver true si los dos números son iguales. 
