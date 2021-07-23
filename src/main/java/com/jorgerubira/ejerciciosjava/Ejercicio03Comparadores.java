4a18f460b45e37c9e0c1ba21ada8f1bfd95de314
Javier A <javialiaga2000@gmail.com>
2021-07-23 12:37:16 +0200

    tests

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
@@ -8,35 +8,63 @@ public class Ejercicio03Comparadores {
     //Si no devuelve false
     //Si se envia null en cualquiera de ellos devuelve false
     public boolean compararEnteros(Integer i1, Integer i2){
-        throw new RuntimeException("Pendiente de hacer");
+        if(i1 != null && i2 != null && i1.intValue() == i2.intValue()){
+            return true;    
+        } else{
+            return false;
+        }
     }
     
     //Debe devolver true si los dos enteros contienen el mismo valor.
     //Si no devuelve false
     //Si se envia null en cualquiera de ellos devuelve false
     public boolean compararEnteroConLong(Integer i1, Long i2){
-        throw new RuntimeException("Pendiente de hacer");
+        if(i1 != null && i2 != null && i1.longValue() == i2.longValue()){
+            return true;
+        }else{
+            return false;
+        } 
     }    
 
     //Debe devolver true si el texto que se ha enviado en un número. 
     //Si no devuelve false
     //Si se envia null devuelve false
-    public boolean comprobarNumero(String numero){
-        throw new RuntimeException("Pendiente de hacer");
+    public boolean comprobarNumero(String numero){ 
+        try{
+            Double.parseDouble(numero);
+        }catch(Exception e){
+            return false;
+        }
+        return true; 
     }    
     
     //Debe devolver true si los dos números son iguales. 
     //Si no devuelve false
     //Si se envia null en cualquier de ellos devuelve false
     public boolean comprobarOptionalConInteger(Optional<Integer> valor1, Integer valor2){
-        throw new RuntimeException("Pendiente de hacer");
+        if(valor1.isPresent() && valor2 != null && valor1.get().equals(valor2)){
+           return true;
+        } else{
+            return false;
+        }
     }    
 
     //Debe devolver true si los dos números son iguales. 
     //Si no devuelve false
     //Si los dos tienen null devuelve false.
     public boolean comprobarOptionalesIntegerYDouble(Optional<Integer> valor1, Optional<Double> valor2){
-        throw new RuntimeException("Pendiente de hacer");
+        
+        if(valor1.isPresent() && valor2.isPresent()){
+            Optional<Double> valor1Double = Optional.of(Double.valueOf(valor1.get()));
+            
+            if((valor1Double).equals(valor2)){
+                return true;
+            }else{
+             return false;   
+            }
+        } else{
+            return false;
+        }
     }    
 
 }
