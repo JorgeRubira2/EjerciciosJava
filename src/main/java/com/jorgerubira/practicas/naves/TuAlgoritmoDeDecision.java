0b867ee9fd3d2f9eec12c27ae5a531ce2cc770bc
Jose <martosmartinjose@gmail.com>
2021-07-25 14:56:36 +0200

    nave sin viento bien con viento regulin, ejercicio 03 realizado

--- src/main/java/com/jorgerubira/practicas/naves/TuAlgoritmoDeDecision.java
+++ src/main/java/com/jorgerubira/practicas/naves/TuAlgoritmoDeDecision.java
@@ -7,13 +7,54 @@ package com.jorgerubira.practicas.naves;
 
 /**
  *
- * @author PC
+    private boolean viento=false;
+    private double fuerzaViento=(Math.random()-0.5)/3;
+    
+    private double altura=0;
+    private double metrosRecorridos=0;
+    private int velocidad=0;
+    private double anguloNave=0;
+    
+    private int giro=0;
+    private boolean propulsion=false;
+    
+    private double dx=0;
+    private double dy=0;
  */
 public class TuAlgoritmoDeDecision implements IDecision {
 
     @Override
     public void decision(INave n, int segundos) {
-        throw new UnsupportedOperationException("Pendiente de hacer."); 
+        
+        //por mejorar segun cantidad de viento y pa donde
+        if (n.getMetrosRecorridos()==0){
+            n.girarMando(45);
+            n.setPropulsion(true);
+        }else if(n.getMetrosRecorridos()>150 && n.getMetrosRecorridos()<600) {
+            n.setPropulsion(false);
+            n.girarMando(-25);
+        }else if(n.getMetrosRecorridos()>600 && n.getMetrosRecorridos()<700) {
+            n.setPropulsion(true);
+            n.girarMando(-45);
+        }else if(n.getMetrosRecorridos()>700 && n.getMetrosRecorridos()<1000) {
+            n.setPropulsion(false);
+            n.girarMando(10);
+        }else if (n.getAltura()<220 && n.getAltura()>50){
+            n.girarMando(20);
+            n.setPropulsion(true);
+        }
+        /*//Sin viento
+        if (n.getMetrosRecorridos()==0){
+            n.girarMando(45);
+            n.setPropulsion(true);
+        }else if(n.getVelocidad()==185) {
+            n.setPropulsion(false);
+            n.girarMando(-25);
+        }else if (n.getAltura()<220 && n.getAltura()>50){
+            n.setPropulsion(true);
+        }*/
+        
+        //throw new UnsupportedOperationException("Pendiente de hacer."); 
     }
     
 }
