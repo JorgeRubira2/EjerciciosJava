dfc8442a770ff300ee795f4fca50f2f38a1e19cd
Aran <aranzazu_90@hotmail.es>
2021-07-27 14:00:34 +0200

    ejercicios04

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio04Colecciones.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio04Colecciones.java
@@ -2,6 +2,8 @@ package com.jorgerubira.ejerciciosjava;
 
 import com.jorgerubira.ejerciciosjava.pojo.Persona;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
@@ -72,106 +74,137 @@ public class Ejercicio04Colecciones {
             }
         }
 
-        return enLista.size(); 
+        return enLista.size();
     }
 
     /**
-     * Contar cuantos elementos del Hashtable son de tipo Integer.
-     * Para comprobar si un valor es de tipo Integer utilizar instanceOf
+     * Contar cuantos elementos del Hashtable son de tipo Integer. Para
+     * comprobar si un valor es de tipo Integer utilizar instanceOf
      */
-    public int contarIntegers(Map<String, Object> tabla){
+    public int contarIntegers(Map<String, Object> tabla) {
+
+        int contador = 0;
+
+        for (Object a : tabla.values()) {
+            if (a instanceof Integer) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+    /**
+     * Borrar todas las personas del Map que sean de la ciudad Huesca. No hace
+     * falta verificar si valen nulo.
+     */
+    public void borrarPersonasHuescaDeLista(List<Persona> listaPersonas) {
+
+        for (int i = listaPersonas.size() - 1; i >= 0; i--) {
+            if (listaPersonas.get(i).getCiudad() == "Huesca") {
+                listaPersonas.remove(i);
+            }
+        }
+    }
+
+    /**
+     * Borrar todas las personas del Map que sean de la ciudad Huesca. No hace
+     * falta verificar si valen nulo.
+     *
+     */
+    public void borrarPersonasHuescaDeMapa(Map<String, Persona> listaPersonas) {
+
+       /* Set<Persona> guardar = new HashSet<Persona>();
+
+        for (String a : listaPersonas.keySet()) {
+            if (listaPersonas.get(a).getCiudad().equals("Huesca")) {
+                guardar.add(listaPersonas.get(a));
+
+            }
+
+        }
+
+        for (int i = 0; i < guardar.size(); i++) {
+            listaPersonas.remove(i);
+
+        }
+*/
+       
+    }
+
+    /**
+     * Si la persona que va a ser atendida en la cola tiene menos de 5 artículos
+     * en la compra o no tiene compra, añadimos la persona personaNueva al
+     * final. La persona tiene una compra como atributo pero puede valer null
+     * también si no tiene compra. No hace falta verificar si valen nulo. Pista:
+     * para ver que persona va a salir pero sin sacarla utilizar peek
+     */
+    public void entrarPersonaALaCola(Queue<Persona> colaPersonas, Persona personaNueva) {
         
         
         throw new RuntimeException("Pendiente de hacer");
     }
-    
+
     /**
-     * Borrar todas las personas del Map que sean de la ciudad Huesca.
-     * No hace falta verificar si valen nulo.
+     * Genera una Lista a partir de un vector de datos primarios. No hace falta
+     * verificar si valen nulo. Pista: Utilizar el método Arrays.asList
+     * <<Devuelve una lista inmutable
      */
-    public void borrarPersonasHuescaDeLista(List<Persona> listaPersonas){
+    public List<Integer> generarLista(int valores[]) {
         throw new RuntimeException("Pendiente de hacer");
     }
-    
+
     /**
-     * Borrar todas las personas del Map que sean de la ciudad Huesca.
-     * No hace falta verificar si valen nulo.
-     * 
+     * Genera un ArrayList a partir de un vector de datos primarios. No hace
+     * falta verificar si valen nulo.
      */
-    public void borrarPersonasHuescaDeMapa(Map<String, Persona> listaPersonas){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Si la persona que va a ser atendida en la cola tiene menos de 5 artículos en la compra o no tiene compra, añadimos la persona personaNueva al final.
-     * La persona tiene una compra como atributo pero puede valer null también si no tiene compra.
-     * No hace falta verificar si valen nulo.
-     * Pista: para ver que persona va a salir pero sin sacarla utilizar peek
-     */    
-    public void entrarPersonaALaCola(Queue<Persona> colaPersonas, Persona personaNueva){
+    public ArrayList<Integer> generarArrayList(int valores[]) {
         throw new RuntimeException("Pendiente de hacer");
     }
 
     /**
-     * Genera una Lista a partir de un vector de datos primarios. 
-     * No hace falta verificar si valen nulo.
-     * Pista: Utilizar el método Arrays.asList <<Devuelve una lista inmutable
-     */    
-    public List<Integer> generarLista(int valores[]){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Genera un ArrayList a partir de un vector de datos primarios. 
-     * No hace falta verificar si valen nulo.
-     */    
-    public ArrayList<Integer> generarArrayList(int valores[]){
+     * Devuelve el texto "Mineral" si el objeto está en el conjunto de minerales
+     * o "Organico" si esta en el conjunto de orgánico. Devuelve "" si no es ni
+     * mineral y orgánico. No hace falta verificar si valen nulo.
+     */
+    public String catalogar(String objeto, Set<String> minerales, Set<String> organico) {
         throw new RuntimeException("Pendiente de hacer");
     }
 
     /**
-     * Devuelve el texto "Mineral" si el objeto está en el conjunto de minerales o "Organico" si esta en el conjunto de orgánico.
-     * Devuelve "" si no es ni mineral y orgánico.
-     * No hace falta verificar si valen nulo.
-     */    
-    public String catalogar(String objeto, Set<String> minerales, Set<String> organico){
+     * Devuelve el Set donde se almacena que palabras sirven para nombrar frutas
+     * y colores. No hace falta verificar si valen nulo. Pista: Para la
+     * interseccion utilizar retainAll.
+     */
+    public Set<String> coincidencias(Set<String> frutas, Set<String> colores) {
         throw new RuntimeException("Pendiente de hacer");
-    }    
+    }
 
     /**
-     * Devuelve el Set donde se almacena que palabras sirven para nombrar frutas y colores.
-     * No hace falta verificar si valen nulo.
-     * Pista: Para la interseccion utilizar retainAll. 
-     */    
-    public Set<String> coincidencias(Set<String> frutas, Set<String> colores){
+     * Recibimos un Map con las notas de una lista de personas. En la clave se
+     * almacena el nombre de la persona y el valor es su nota Se pide hacer un
+     * algoritmo que devuelva un List con los nombres de las peronas que han
+     * sacado >=5 No hace falta verificar si valen nulo.
+     */
+    public List<String> aprobados(Map<String, Integer> notas) {
         throw new RuntimeException("Pendiente de hacer");
-    }        
+    }
 
     /**
-     * Recibimos un Map con las notas de una lista de personas. En la clave se almacena el nombre de la persona y el valor es su nota
-     * Se pide hacer un algoritmo que devuelva un List con los nombres de las peronas que han sacado >=5
-     * No hace falta verificar si valen nulo.
-     */    
-    public List<String> aprobados(Map<String, Integer> notas){
+     * Devuelve un Map cuya clave es el nombre de la persona y el valor es el
+     * número de articulos totales que hay. Nota la persona puede aparecer
+     * varias veces en la lista, en ese caso se irá sumando No hace falta
+     * verificar si valen nulo.
+     */
+    public Map<String, Integer> totalProductos(List<Persona> personas) {
         throw new RuntimeException("Pendiente de hacer");
-    } 
+    }
 
-    
     /**
-     * Devuelve un Map cuya clave es el nombre de la persona y el valor es el número de articulos totales que hay. 
-     * Nota la persona puede aparecer varias veces en la lista, en ese caso se irá sumando 
-     * No hace falta verificar si valen nulo.
-     */    
-    public Map<String, Integer> totalProductos(List<Persona> personas){
-        throw new RuntimeException("Pendiente de hacer");
-    }     
-    
-    
-    /**
-     * Añade multiples valores a la lista.
-     * No hace falta verificar si valen nulo.
-     */    
-    public void annadirElementosMultiples(List<Integer> destino, int ... valores){
+     * Añade multiples valores a la lista. No hace falta verificar si valen
+     * nulo.
+     */
+    public void annadirElementosMultiples(List<Integer> destino, int... valores) {
         throw new RuntimeException("Pendiente de hacer");
     }
 
