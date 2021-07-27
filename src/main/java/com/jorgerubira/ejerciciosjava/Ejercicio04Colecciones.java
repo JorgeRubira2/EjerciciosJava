7e49263c3d8990a1eba0e1ecb0d7ec4e6ad19c8f
raulf <raulf@DESKTOP-A3LPG8M>
2021-07-27 15:03:36 +0200

    Ejercicio 04 terminado

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio04Colecciones.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio04Colecciones.java
@@ -2,16 +2,17 @@ package com.jorgerubira.ejerciciosjava;
 
 import com.jorgerubira.ejerciciosjava.pojo.Persona;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
 public class Ejercicio04Colecciones {
-    
+
     /**
-     * Si el valor no es negativo (>=0) insertarlo a la lista de destino.
-     * No hace falta comprobar si destino es nulo.
+     * Si el valor no es negativo (>=0) insertarlo a la lista de destino. No
+     * hace falta comprobar si destino es nulo.
      */
     public void insertarElementoEnLista(long valor, List<Long> destino) {
         if (valor >= 0) {
@@ -34,8 +35,8 @@ public class Ejercicio04Colecciones {
      * hace falta verificar si valen nulo.
      */
     public void copiar(List<Double> origen, List<Double> destino) {
-        for(int n=0;n<origen.size();n++){
-            if (origen.get(n)>=0){
+        for (int n = 0; n < origen.size(); n++) {
+            if (origen.get(n) >= 0) {
                 destino.add(origen.get(n));
             }
         }
@@ -47,119 +48,190 @@ public class Ejercicio04Colecciones {
      */
     public int contarElementosEnSet(List<Integer> lista, Set<Integer> enLista) {
         int resultado = 0;
-         for(int n=0;n<lista.size();n++){
-             if (enLista.contains(lista.get(n))) {
+        for (int n = 0; n < lista.size(); n++) {
+            if (enLista.contains(lista.get(n))) {
                 resultado++;
-             }
+            }
         }
-         return resultado;
+        return resultado;
     }
-    
+
     /**
-     * Contar cuantos elementos no repetidos del List lista están en el Set. Devolver el resultado.
-     * No hace falta verificar si valen nulo.
-     * Pista. Al encontrar un elemento sacarlo del Set para que no lo vuelva a contar.
+     * Contar cuantos elementos no repetidos del List lista están en el Set.
+     * Devolver el resultado. No hace falta verificar si valen nulo. Pista. Al
+     * encontrar un elemento sacarlo del Set para que no lo vuelva a contar.
      */
-    public int contarElementosEnSetNoRepetidos(List<Integer> lista, Set<Integer> enLista){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-    
+    public int contarElementosEnSetNoRepetidos(List<Integer> lista, Set<Integer> enLista) {
+        int resultado = 0;
+        for (int n = 0; n < lista.size(); n++) {
+            if (enLista.contains(lista.get(n))) {
+                enLista.remove(lista.get(n));
+                resultado++;
+            }
+        }
+        return resultado;
+    }
+
     /**
-     * Contar cuantos elementos del Hashtable son de tipo Integer.
-     * Para comprobar si un valor es de tipo Integer utilizar instanceOf
+     * Contar cuantos elementos del Hashtable son de tipo Integer. Para
+     * comprobar si un valor es de tipo Integer utilizar instanceOf
      */
-    public int contarIntegers(Map<String, Object> tabla){
-        throw new RuntimeException("Pendiente de hacer");
+    public int contarIntegers(Map<String, Object> tabla) {
+        int resultado = 0;
+        for (Object valor : tabla.values()) {
+            if (valor instanceof Integer) {
+                resultado++;
+            }
+        }
+        return resultado;
     }
-    
+
     /**
-     * Borrar todas las personas del Map que sean de la ciudad Huesca.
-     * No hace falta verificar si valen nulo.
+     * Borrar todas las personas del Map que sean de la ciudad Huesca. No hace
+     * falta verificar si valen nulo.
      */
-    public void borrarPersonasHuescaDeLista(List<Persona> listaPersonas){
-        throw new RuntimeException("Pendiente de hacer");
+    public void borrarPersonasHuescaDeLista(List<Persona> listaPersonas) {
+        for (int n = listaPersonas.size() - 1; n > 0; n--) {
+            if (listaPersonas.get(n).getCiudad().equals("Huesca")) {
+                listaPersonas.remove(n);
+            }
+        }
     }
-    
+
     /**
-     * Borrar todas las personas del Map que sean de la ciudad Huesca.
-     * No hace falta verificar si valen nulo.
-     * 
+     * Borrar todas las personas del Map que sean de la ciudad Huesca. No hace
+     * falta verificar si valen nulo. (Se han puesto comentarios al ser una
+     * parte algo más compleja)
      */
-    public void borrarPersonasHuescaDeMapa(Map<String, Persona> listaPersonas){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Si la persona que va a ser atendida en la cola tiene menos de 5 artículos en la compra o no tiene compra, añadimos la persona personaNueva al final.
-     * La persona tiene una compra como atributo pero puede valer null también si no tiene compra.
-     * No hace falta verificar si valen nulo.
-     * Pista: para ver que persona va a salir pero sin sacarla utilizar peek
-     */    
-    public void entrarPersonaALaCola(Queue<Persona> colaPersonas, Persona personaNueva){
-        throw new RuntimeException("Pendiente de hacer");
+    public void borrarPersonasHuescaDeMapa(Map<String, Persona> listaPersonas) {
+        List<String> list = new ArrayList<>(); //Creamos ArrayList para guardar las Keys de listaPersonas
+        for (String key : listaPersonas.keySet()) { //Recorremos listaPersonas
+            if ((listaPersonas.get(key).getCiudad()).equals("Huesca")) { //Si esa persona tiene como ciudad "Huesca", añadimos su key al ArrayList
+                list.add(key);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) { //Recorremos el ArrayList y eliminamos los elementos de ListaPersonas con esa key
+            listaPersonas.remove(list.get(i));
+        }
     }
 
     /**
-     * Genera una Lista a partir de un vector de datos primarios. 
-     * No hace falta verificar si valen nulo.
-     * Pista: Utilizar el método Arrays.asList <<Devuelve una lista inmutable
-     */    
-    public List<Integer> generarLista(int valores[]){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Genera un ArrayList a partir de un vector de datos primarios. 
-     * No hace falta verificar si valen nulo.
-     */    
-    public ArrayList<Integer> generarArrayList(int valores[]){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-
-    /**
-     * Devuelve el texto "Mineral" si el objeto está en el conjunto de minerales o "Organico" si esta en el conjunto de orgánico.
-     * Devuelve "" si no es ni mineral y orgánico.
-     * No hace falta verificar si valen nulo.
-     */    
-    public String catalogar(String objeto, Set<String> minerales, Set<String> organico){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-
-    /**
-     * Devuelve el Set donde se almacena que palabras sirven para nombrar frutas y colores.
-     * No hace falta verificar si valen nulo.
-     * Pista: Para la interseccion utilizar retainAll. 
-     */    
-    public Set<String> coincidencias(Set<String> frutas, Set<String> colores){
-        throw new RuntimeException("Pendiente de hacer");
-    }        
-
-    /**
-     * Recibimos un Map con las notas de una lista de personas. En la clave se almacena el nombre de la persona y el valor es su nota
-     * Se pide hacer un algoritmo que devuelva un List con los nombres de las peronas que han sacado >=5
-     * No hace falta verificar si valen nulo.
-     */    
-    public List<String> aprobados(Map<String, Integer> notas){
-        throw new RuntimeException("Pendiente de hacer");
-    } 
-
-    
-    /**
-     * Devuelve un Map cuya clave es el nombre de la persona y el valor es el número de articulos totales que hay. 
-     * Nota la persona puede aparecer varias veces en la lista, en ese caso se irá sumando 
-     * No hace falta verificar si valen nulo.
-     */    
-    public Map<String, Integer> totalProductos(List<Persona> personas){
-        throw new RuntimeException("Pendiente de hacer");
-    }     
-    
-    
-    /**
-     * Añade multiples valores a la lista.
-     * No hace falta verificar si valen nulo.
-     */    
-    public void annadirElementosMultiples(List<Integer> destino, int ... valores){
-        throw new RuntimeException("Pendiente de hacer");
+     * Si la persona que va a ser atendida en la cola tiene menos de 5 artículos
+     * en la compra o no tiene compra, añadimos la persona personaNueva al
+     * final. La persona tiene una compra como atributo pero puede valer null
+     * también si no tiene compra. No hace falta verificar si valen nulo. Pista:
+     * para ver que persona va a salir pero sin sacarla utilizar peek
+     */
+    public void entrarPersonaALaCola(Queue<Persona> colaPersonas, Persona personaNueva) {
+        if (!colaPersonas.isEmpty()) {
+            if (colaPersonas.peek().getCesta().isEmpty() || colaPersonas.peek().getCesta().get().getTotalArticulos() < 5) {
+                colaPersonas.add(personaNueva);
+            }
+        } else {
+            colaPersonas.add(personaNueva);
+        }
     }
 
+    /**
+     * Genera una Lista a partir de un vector de datos primarios. No hace falta
+     * verificar si valen nulo. Pista: Utilizar el método Arrays.asList Devuelve
+     * una lista inmutable
+     */
+    public List<Integer> generarLista(int valores[]) {
+        List<Integer> list = new ArrayList<>();
+        for (int numero : valores) {
+            list.add(numero);
+        }
+        return list;
+    }
+
+    /**
+     * Genera un ArrayList a partir de un vector de datos primarios. No hace
+     * falta verificar si valen nulo.
+     */
+    public ArrayList<Integer> generarArrayList(int valores[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int numero : valores) {
+            list.add(numero);
+        }
+        return list;
+    }
+
+    /**
+     * Devuelve el texto "Mineral" si el objeto está en el conjunto de minerales
+     * o "Organico" si esta en el conjunto de orgánico. Devuelve "" si no es ni
+     * mineral y orgánico. No hace falta verificar si valen nulo.
+     */
+    public String catalogar(String objeto, Set<String> minerales, Set<String> organico) {
+        if (minerales.contains(objeto)) {
+            return "Mineral";
+        } else if (organico.contains(objeto)) {
+            return "Organico";
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Devuelve el Set donde se almacena que palabras sirven para nombrar frutas
+     * y colores. No hace falta verificar si valen nulo. Pista: Para la
+     * interseccion utilizar retainAll.
+     */
+    public Set<String> coincidencias(Set<String> frutas, Set<String> colores) {
+        frutas.retainAll(colores);
+        return frutas;
+    }
+
+    /**
+     * Recibimos un Map con las notas de una lista de personas. En la clave se
+     * almacena el nombre de la persona y el valor es su nota Se pide hacer un
+     * algoritmo que devuelva un List con los nombres de las peronas que han
+     * sacado >=5 No hace falta verificar si valen nulo.
+     */
+    public List<String> aprobados(Map<String, Integer> notas) {
+        List<String> list = new ArrayList<>(); //Creamos una lista para guardar los nombres de los alumnos aprobados
+        for (String nombre : notas.keySet()) { //Recorremos el Map de los alumnos con sus notas
+            if (notas.get(nombre) >= 5) { //Si es mayor de 5 la nota, se añade a la lista
+                list.add(nombre);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Devuelve un Map cuya clave es el nombre de la persona y el valor es el
+     * número de articulos totales que hay. Nota la persona puede aparecer
+     * varias veces en la lista, en ese caso se irá sumando No hace falta
+     * verificar si valen nulo.
+     */
+    public Map<String, Integer> totalProductos(List<Persona> personas) {
+        Map<String, Integer> list = new HashMap<>(); //Creamos una lista donde guardaremos a las personas
+        for (Persona person : personas) { //Recorremos el List de Persona
+            if (list.containsKey(person.getNombre())) { //Comprobamos que esa persona tenga ya una Key en nuestro list
+                int cesta = list.get(person.getNombre()); //Añadimos el valor de la cesta actual
+                if (person.getCesta().isPresent()) { //Comprobamos que haya algún valor 
+                    list.replace(person.getNombre(), cesta + person.getCesta().get().getTotalArticulos()); //Remplazamos añadiendo nombre y la cesta+el total de la nueva cesta
+                } else {
+                    list.replace(person.getNombre(), cesta); //Reemplazamos añadiendo el nombre y la cesta actual
+                }
+            } else {
+                if (person.getCesta().isPresent()) { //Comprobamos que haya algún valor
+                    list.put(person.getNombre(), person.getCesta().get().getTotalArticulos()); //Al haber valor, añadimos a la persona con su cesta actual
+                } else {
+                    list.put(person.getNombre(), 0); //Si no hay ningún valor, añadimos a esa nueva persona con 0 artículos en la cesta
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Añade multiples valores a la lista. No hace falta verificar si valen
+     * nulo.
+     */
+    public void annadirElementosMultiples(List<Integer> destino, int... valores) {
+        for (int i = 0; i < valores.length; i++) {
+            destino.add(valores[i]);
+        }
+    }
 }
