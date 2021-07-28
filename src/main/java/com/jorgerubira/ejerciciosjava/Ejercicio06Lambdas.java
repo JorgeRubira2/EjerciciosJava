e4f7c04c14476211409b6982d0243a8d2699f5ac
raulf <raulf@DESKTOP-A3LPG8M>
2021-07-28 13:09:43 +0200

    Se han completado varios métodos del Ejercicio06

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio06Lambdas.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio06Lambdas.java
@@ -1,4 +1,3 @@
-
 package com.jorgerubira.ejerciciosjava;
 
 import com.jorgerubira.ejerciciosjava.interfaces.IComparadorPersonaCompra;
@@ -14,151 +13,201 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
-
 public class Ejercicio06Lambdas {
-    
-    /**
-     * Devolver una función Comparator con Lambdas que compare dos enteros 
-     * y devuelva si el primero parametro1 es menor que el parametro2
-     * No hace falta comprobar los valores nulos.
-     */
-    public Comparator<Integer> compararIntegers(){
-        return (v1, v2) -> v1 - v2; 
-    } 
 
     /**
-     * Devolver una función Comparator con Lambdas que compare dos Strings 
-     * y devuelva si el primero parametro1 es menor que el parametro2
-     * Los valores null se considerarán los más bajos a nivel de comparación
+     * Devolver una función Comparator con Lambdas que compare dos enteros y
+     * devuelva si el primero parametro1 es menor que el parametro2 No hace
+     * falta comprobar los valores nulos.
      */
-    public Comparator<String> compararStrings(){
-        throw new RuntimeException("Pendiente de hacer");
-    } 
-
-    /**
-     * Devolver una función Comparator con Lambdas que compare dos Personas 
-     * para ordenar por edad de menor a mayor.
-     * No hace falta comprobar personas con valor nulo.
-     */
-    public Comparator<Persona> compararPersonasPorEdadAscendente(){
-        throw new RuntimeException("Pendiente de hacer");
-    } 
-    
-    /**
-     * Devolver una función Comparator con Lambdas que compare dos Personas 
-     * para ordenar por edad de mayor a menor
-     */
-    public Comparator<Persona> compararPersonasPorEdadDescendente(){
-        throw new RuntimeException("Pendiente de hacer");
-    }     
-    
-    /**
-     * Devolver una función Comparator con Lambdas que compare dos Personas para ordenar 
-     * ascendentemente por la ciudad y los que tengan la misma ciudad por nombre.
-     * No hace falta comprobar los nulos.
-     */
-    public Comparator<Persona> compararPersonasPorCiudadYNombre(){
-        throw new RuntimeException("Pendiente de hacer");
-    }     
-    
-    /**
-     * Devolver una función Predicate que permita saber si la persona es de Huesca.
-     * tener en cuenta también valores nulos en la ciudad.
-     */
-    public Predicate<Persona> esLaPersonaDeHuesca(){
-        throw new RuntimeException("Pendiente de hacer");
+    public Comparator<Integer> compararIntegers() {
+        return (v1, v2) -> {
+            return v1 - v2;
+        };
     }
 
     /**
-     * Devolver una función Predicate que diga si la persona esta en edad laboral:
-     * Mayor o igual que 16 y menor que 64
+     * Devolver una función Comparator con Lambdas que compare dos Strings y
+     * devuelva si el primero parametro1 es menor que el parametro2 Los valores
+     * null se considerarán los más bajos a nivel de comparación
      */
-    public Predicate<Persona> esEnEdadLaboral(){
-        throw new RuntimeException("Pendiente de hacer");
+    public Comparator<String> compararStrings() {
+        return (v1, v2) -> {
+            if (v1 == null && v2 == null) {
+                return 0;
+            } else if (v1 == null) {
+                return -1;
+
+            } else if (v2 == null) {
+                return 1;
+            }
+            return v1.compareTo(v2);
+        };
+    }
+
+    /**
+     * Devolver una función Comparator con Lambdas que compare dos Personas para
+     * ordenar por edad de menor a mayor. No hace falta comprobar personas con
+     * valor nulo.
+     */
+    public Comparator<Persona> compararPersonasPorEdadAscendente() {
+        return (v1, v2) -> {
+            return v1.getEdad() - v2.getEdad();
+        };
+    }
+
+    /**
+     * Devolver una función Comparator con Lambdas que compare dos Personas para
+     * ordenar por edad de mayor a menor
+     */
+    public Comparator<Persona> compararPersonasPorEdadDescendente() {
+        return (v1, v2) -> {
+            return v2.getEdad() - v1.getEdad();
+        };
+    }
+
+    /**
+     * Devolver una función Comparator con Lambdas que compare dos Personas para
+     * ordenar ascendentemente por la ciudad y los que tengan la misma ciudad
+     * por nombre. No hace falta comprobar los nulos.
+     */
+    public Comparator<Persona> compararPersonasPorCiudadYNombre() {
+        return (v1, v2) -> {
+            if (v1.getCiudad().compareTo(v2.getCiudad()) != 0) {
+                return v1.getCiudad().compareTo(v2.getCiudad());
+            } else {
+                return v1.getNombre().compareTo(v2.getNombre());
+            }
+        };
+    }
+
+    /**
+     * Devolver una función Predicate que permita saber si la persona es de
+     * Huesca. tener en cuenta también valores nulos en la ciudad.
+     */
+    public Predicate<Persona> esLaPersonaDeHuesca() {
+        return (v1) -> {
+            if (v1.getCiudad() == "Huesca") {
+                return true;
+            } else if (v1.getCiudad() == null) {
+                return false;
+            } else {
+                return false;
+            }
+        };
+    }
+
+    /**
+     * Devolver una función Predicate que diga si la persona esta en edad
+     * laboral: Mayor o igual que 16 y menor que 64
+     */
+    public Predicate<Persona> esEnEdadLaboral() {
+        return (v1) -> {
+            if (v1.getEdad() >= 16 && v1.getEdad() < 64) {
+                return true;
+            } else {
+                return false;
+            }
+        };
     }
 
     /**
      * Devolver una función Function que devuelva el nombre de las personas.
      */
-    public Function<Persona, String> obtenerNombreDePersonas(){
-        throw new RuntimeException("Pendiente de hacer");
+    public Function<Persona, String> obtenerNombreDePersonas() {
+        return (v1) -> {
+            return v1.getNombre();
+        };
     }
 
     /**
-     * Devolver una función Function que devuelva la compra (Opcional) de las personas.
+     * Devolver una función Function que devuelva la compra (Opcional) de las
+     * personas.
      */
-    public Function<Persona, Optional<Compra>> obtenerCompraOpcionalDePersonas(){
-        throw new RuntimeException("Pendiente de hacer");
+    public Function<Persona, Optional<Compra>> obtenerCompraOpcionalDePersonas() {
+        return (v1) -> {
+            return v1.getCesta();
+        };
     }
 
     /**
-     * Devolver una función Function que devuelva la compra (Opcional) de las personas.
-     * Devolver null si no tiene compra.
+     * Devolver una función Function que devuelva la compra (Opcional) de las
+     * personas. Devolver null si no tiene compra.
      */
-    public Function<Persona, Compra> obtenerCompraDePersonas(){
-        throw new RuntimeException("Pendiente de hacer");
+    public Function<Persona, Compra> obtenerCompraDePersonas() {
+        return (v1) -> {
+            if (v1.getCesta().isPresent()) {
+                return v1.getCesta().get();
+            } else {
+                return null;
+            }
+        };
     }
 
     /**
      * Crear una función Consumer que incremente la edad de las personas en 1
      */
-    public Consumer<Persona> incrementarEdad(){
+    public Consumer<Persona> incrementarEdad() {
+        return (v1) -> {
+            v1.setEdad(v1.getEdad() + 1);
+        };
+    }
+
+    /**
+     * Crear una función BiConsumer que copie la compra de la segunda persona a
+     * la primera. La cesta de la segunda persona debe quedar empty. BiConsumer
+     * recibe dos parametros y lleva void
+     */
+    public BiConsumer<Persona, Persona> moverCompraAlInicio() {
         throw new RuntimeException("Pendiente de hacer");
     }
 
     /**
-     * Crear una función BiConsumer que copie la compra de la segunda persona a la primera.
-     * La cesta de la segunda persona debe quedar empty.
-     * BiConsumer recibe dos parametros y lleva void
-     */
-    public BiConsumer<Persona, Persona> moverCompraAlInicio(){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
      * Devuelve una compra vacia.
      */
-    public Supplier<Optional<Compra>> generarCompraVacia(){
+    public Supplier<Optional<Compra>> generarCompraVacia() {
         throw new RuntimeException("Pendiente de hacer");
     }
 
     /**
      * Devuelve una compra con 0 unidades y false en el carro.
      */
-    public Supplier<Compra> generarCompra0Unidades(){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Devuelve la cadena enviada en mayúsculas
-     * No hace falta comprobar valores nulos.
-     */
-    public UnaryOperator<String> convertirAMayusculas(){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-    
-    /**
-     * Devuelve la suma de dos enteros.
-     * Recibe dos parametros int y devuelve un parametro int
-     */
-    public IntBinaryOperator sumar(){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-    
-    /**
-     * Devuelve un comparador para ver si la compra de la persona es mayor que la compra recibida.
-     * Se comparará el número de artículos y si tiene mas devolverá true
-     * Comprobar si la compra recibida vale empty. En ese caso será equivalente a 0 unidades.
-     */
-    public IComparadorPersonaCompra miCompraEsMayorQueOtra(){
+    public Supplier<Compra> generarCompra0Unidades() {
         throw new RuntimeException("Pendiente de hacer");
     }
 
     /**
-     * Devuelve un comparador para ver si la compra de una persona tiene las mismas unidades que otra compra.
-     * Tener el valor empty equivale a tener 0 unidades.
+     * Devuelve la cadena enviada en mayúsculas No hace falta comprobar valores
+     * nulos.
      */
-    public IComparadorPersonaCompra igualNumeroDeUnidades(){
+    public UnaryOperator<String> convertirAMayusculas() {
+        throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve la suma de dos enteros. Recibe dos parametros int y devuelve un
+     * parametro int
+     */
+    public IntBinaryOperator sumar() {
+        throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve un comparador para ver si la compra de la persona es mayor que
+     * la compra recibida. Se comparará el número de artículos y si tiene mas
+     * devolverá true Comprobar si la compra recibida vale empty. En ese caso
+     * será equivalente a 0 unidades.
+     */
+    public IComparadorPersonaCompra miCompraEsMayorQueOtra() {
+        throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve un comparador para ver si la compra de una persona tiene las
+     * mismas unidades que otra compra. Tener el valor empty equivale a tener 0
+     * unidades.
+     */
+    public IComparadorPersonaCompra igualNumeroDeUnidades() {
         throw new RuntimeException("Pendiente de hacer");
     }
 
