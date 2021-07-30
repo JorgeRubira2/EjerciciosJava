e5bf617cb688cc959573572079b5ac81f580a9c2
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-07-30 14:26:14 +0200

    Ejercicio07 me quedan 3

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio07StreamsNivelDificil.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio07StreamsNivelDificil.java
@@ -3,9 +3,17 @@ package com.jorgerubira.ejerciciosjava;
 import com.jorgerubira.ejerciciosjava.pojo.Compra;
 import com.jorgerubira.ejerciciosjava.pojo.Pair;
 import com.jorgerubira.ejerciciosjava.pojo.Persona;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
+import java.util.HashMap;
 
 
 public class Ejercicio07StreamsNivelDificil {
@@ -16,7 +24,22 @@ public class Ejercicio07StreamsNivelDificil {
      * Debe devolver las personas que tengan al menos dos de los tres tipos de estudios.
      */
     public Set<Persona> personaEnDosGrupos(Set<Persona> personasGradoMedio, Set<Persona> personasUniversidad, Set<Persona> personasCertificado){
-        throw new RuntimeException("Pendiente de hacer");
+        List<Persona> resultado = new ArrayList();
+        resultado.addAll(personasGradoMedio);
+        resultado.addAll(personasUniversidad);
+        resultado.addAll(personasCertificado);
+        Set<Persona> resultado2 = resultado.stream()
+        		.collect(Collectors.groupingBy(x->x.getNombre()))
+        		.entrySet()
+        		.stream()
+        		.filter(x->x.getValue().size()>=2)
+                .map(x->x.getValue().get(0))
+                .collect(Collectors.toSet())
+                ;
+        return resultado2;
+        
+        
+        
     }
     /**
      * Realización de una compra conjunta. Se ha realizado una compra conjunta y se debe distribuir los articulos entre las personas que llegan a la lista.
@@ -25,7 +48,15 @@ public class Ejercicio07StreamsNivelDificil {
      * Si la persona en el mapa no se le dará ningún artículo y se le pondrá la compra a Empty
      */
     public void repartoDeCompraPersona(Compra compraConjunta, List<Persona> personasARepartirLaCompra, Map<String,Integer> porcentajes){
-        throw new RuntimeException("Pendiente de hacer");
+    	 porcentajes.entrySet().stream().forEach(x -> {
+             personasARepartirLaCompra.forEach(y -> {
+                 if (y.getNombre().equals(x.getKey())) {
+                     int porcentaje = x.getValue();
+                     y.setCesta(new Compra(porcentaje * compraConjunta.getTotalArticulos() / 100, true));
+                 }
+             });
+         }
+         );
     }    
 
     /**
@@ -34,7 +65,15 @@ public class Ejercicio07StreamsNivelDificil {
      * Hacer con un stream.
      */
     public List<Persona> generadorDePersonasAlAzar(int totalPersonas, List<String> nombres){
-        throw new RuntimeException("Pendiente de hacer");
+    	List<Persona> resultado = new ArrayList<>();
+        Collections.nCopies(totalPersonas, new Persona(""))
+                .stream()
+                .forEach(x -> resultado.add(new Persona(nombres.get((int) (Math.random() * nombres.size())))));
+        ;
+        return resultado;
+    	
+        
+        
     }
 
     
@@ -44,7 +83,18 @@ public class Ejercicio07StreamsNivelDificil {
      * Pair esta creado en el código pero existe una librería llamada Javatuples que funciona igual
      */
     public Pair<List<Persona>,List<Persona>> obtenerTuplaPorEdad(List<Persona> nombres, Set<String> descartes){
-        throw new RuntimeException("Pendiente de hacer");
+    	List<Persona> encima18 = new ArrayList<>();
+        List<Persona> debajo18 = new ArrayList<>();
+        nombres.stream()
+                .filter(x -> x.getEdad() >= 18)
+                .filter(x -> !descartes.contains(x.getNombre()))
+                .forEach(x -> encima18.add(new Persona(x.getNombre())));
+        nombres.stream()
+                .filter(x -> x.getEdad() < 18)
+                .filter(x -> !descartes.contains(x.getNombre()))
+                .forEach(x -> debajo18.add(new  Persona(x.getNombre())));
+        Pair<List<Persona>, List<Persona>> resultado = new Pair<> (debajo18, encima18);
+        return resultado;
     }    
     
     /**
@@ -54,14 +104,35 @@ public class Ejercicio07StreamsNivelDificil {
      * Devolver la información ordenada por Nombre
      */
     public List<Persona> devolverDeQuePersonasCorrespondenLasCompras(List<Persona> nombres, Set<Compra> descartes){
-        throw new RuntimeException("Pendiente de hacer");
+    	List<Persona> resultado = new ArrayList<>();
+        nombres.stream()
+                .filter(x ->x.getCesta().isPresent())
+                .sorted((x, y) -> x.getNombre().compareTo(y.getNombre()))
+                .forEach(x ->{
+                    if (descartes.contains(x.getCesta().get())) {
+                        resultado.add(x);
+                    }
+                });
+
+        return resultado;
     }
 
     /*
      * Devolver una lista de personas pero en caso de tener menos de 5 productos dejar la compra a empty.
      */
     public List<Persona> eliminarCompraDePersonas(List<Persona> nombres){
-        throw new RuntimeException("Pendiente de hacer");
+    	List<Persona> resultado = new ArrayList<>();
+        nombres.stream()
+                .forEach(x->{
+                    if (x.getCesta().isPresent() && x.getCesta().get().getTotalArticulos()<5){
+                        x.setCesta(null);
+                    }else if (!x.getCesta().isPresent()){
+                        x.setCesta(null);
+                    }
+                    resultado.add(x);
+                });
+        
+        return resultado;
     }
 
     /*
@@ -76,9 +147,14 @@ public class Ejercicio07StreamsNivelDificil {
      * La clave del mapa será la ciudad y la lista de Personas de cada ciudad .
      */
     public Map<String, List<Persona>> obtenerPersonasDeCadaCiudad(List<Persona> persona){
-        throw new RuntimeException("Pendiente de hacer");
+    	Map<String, List<Persona>> resultado = new HashMap<>();
+        resultado = persona.stream()
+                .collect(Collectors.groupingBy(x->x.getCiudad()))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(x->x.getKey(), x->x.getValue()));
+        return resultado;
     }
-    
     
     /**
      * Devuelve en un mapa la persona con más edad de cada ciudad.
