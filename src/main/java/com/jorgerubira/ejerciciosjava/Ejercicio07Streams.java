a257a9f341cd98b6df9cc86f785022f059bf45be
Aran <aranzazu_90@hotmail.es>
2021-07-30 14:21:00 +0200

    streams

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio07Streams.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio07Streams.java
@@ -1,131 +1,196 @@
-
 package com.jorgerubira.ejerciciosjava;
 
 import com.jorgerubira.ejerciciosjava.pojo.Ciudad;
 import com.jorgerubira.ejerciciosjava.pojo.Compra;
 import com.jorgerubira.ejerciciosjava.pojo.Persona;
 import com.jorgerubira.ejerciciosjava.pojo.RangoEdad;
+import static java.util.Collections.list;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 
 public class Ejercicio07Streams {
 
     /**
-     * Devolver los elementos positivos >=0 del Array origen
-     * No hace falta verificar si valen nulo.
+     * Devolver los elementos positivos >=0 del Array origen No hace falta
+     * verificar si valen nulo.
      */
-    public List<Double> elementosPositivos(List<Double> origen){
-        throw new RuntimeException("Pendiente de hacer");
+    public List<Double> elementosPositivos(List<Double> origen) {
+
+        return origen.stream()
+                .filter((x) -> x >= 0)
+                .collect(toList());
+
+        // throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve el valor maximo de la lista. No hace falta verificar si valen
+     * nulo.
+     */
+    public int maximoElemento(List<Integer> lista) {
+
+        Optional<Integer> maximo = lista.stream()
+                .max((x, y) -> x - y);
+        return maximo.get();
+    }
+
+    /**
+     * Devuelve cuantos elementos no hay repetidos. No hace falta verificar si
+     * valen nulo.
+     */
+    public int contarElementosNoRepetidos(List<Integer> lista) {
+        //map despues de agrupar
+        //gruopingby
+        //contar  cuantas veces aparece cada nuemro
+        //agruparlo por numero
+
+        Map<Integer, Long> agruparRepetidos = lista.stream()
+                .collect(Collectors.groupingBy((x) -> x, Collectors.counting()));
+        long contar = agruparRepetidos.values().stream()
+                .filter((x) -> x == 1)
+                .count();
+
+        return (int) contar;
+        //throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve una lista de las personas que son de Huesca. No hace falta
+     * verificar si valen nulo.
+     */
+    public List<Persona> personasDeHuescaALista(List<Persona> lista) {
+
+        return lista.stream().filter((x) -> x.getCiudad().equals("Huesca"))
+                .collect(Collectors.toList());
+
+        // throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve una lista de las personas que son de Huesca en array de tipo
+     * primario. No hace falta verificar si valen nulo.
+     */
+    public Persona[] personasDeHuescaAArrayBasico(List<Persona> lista) {
+
+        return lista.stream()
+                .filter((x) -> x.getCiudad().equals("Huesca"))
+                .toArray((x) -> new Persona[x]);
+
+        //throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve la persona que tenga más articulos en la cesta. Devuelve empty
+     * si no hay personas. No hace falta verificar si valen nulo.
+     */
+    public Optional<Persona> personasConMasArticulo(List<Persona> lista) {
+
+        Optional<Persona> persona = lista.stream()
+                .max((x, y) -> x.getCesta().orElse(new Compra(0, false)).getTotalArticulos() - y.getCesta().orElse(new Compra(0, false)).getTotalArticulos());
+
+        return persona;
+        //throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve las compras que tienen las personas. Puede haber personas sin
+     * Cesta, estos casos no hay que devolverlos. No hace falta verificar si
+     * valen nulo.
+     */
+    public List<Compra> cestasDeLasPersonas(List<Persona> lista) {
+
+        return lista.stream()
+                .filter(x -> x.getCesta().isPresent())
+                .map(x -> x.getCesta().get())
+                .collect(toList());
+
+        // throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve las edades de las personas. No hace falta verificar si valen
+     * nulo.
+     */
+    public int[] edadesDeLasPersonas(List<Persona> lista) {
+
+        int[] edades = lista.stream()
+                .mapToInt(x -> x.getEdad())
+                .toArray();
+
+        return edades;
+
+        // throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Devuelve cuantas personas hay en cada ciudad. Ciudad contiene dos campos
+     * Nombre de la ciudad y cuantas personas hay de la lista. No hace falta
+     * verificar si valen nulo.
+     */
+    public List<Ciudad> cuantasPersonasHayPorCiudad(List<Persona> lista) {
         
+        
+        Map<String,Long> ciudades= lista.stream()
+                .collect(Collectors.groupingBy(x-> x.getCiudad(), Collectors.counting()));
+        
+        //System.out.println(ciudades);
+        
+        List<Ciudad> personasEnCiudad= ciudades.entrySet()
+                .stream()
+                .map(x-> new Ciudad(x.getKey(), x.getValue().intValue()))
+                .collect(Collectors.toList());
+        
+
+       // throw new RuntimeException("Pendiente de hacer");
+        return personasEnCiudad;
     }
-    
+
     /**
-     * Devuelve el valor maximo de la lista.
-     * No hace falta verificar si valen nulo.
+     * Top 3 clientes. Devuelve los tres clientes que más articulos en la cesta
+     * tienen. No hace falta verificar si valen nulo.
      */
-    public int maximoElemento(List<Integer> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Devuelve cuantos elementos no hay repetidos.
-     * No hace falta verificar si valen nulo.
-     */
-    public int contarElementosNoRepetidos(List<Integer> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Devuelve una lista de las personas que son de Huesca.
-     * No hace falta verificar si valen nulo.
-     */
-    public List<Persona> personasDeHuescaALista(List<Persona> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Devuelve una lista de las personas que son de Huesca en array de tipo primario.
-     * No hace falta verificar si valen nulo.
-     */
-    public Persona[] personasDeHuescaAArrayBasico(List<Persona> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }
-    
-    /**
-     * Devuelve la persona que tenga más articulos en la cesta.
-     * Devuelve empty si no hay personas.
-     * No hace falta verificar si valen nulo.
-     */
-    public Optional<Persona> personasConMasArticulo(List<Persona> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-    
-    /**
-     * Devuelve las compras que tienen las personas.
-     * Puede haber personas sin Cesta, estos casos no hay que devolverlos.
-     * No hace falta verificar si valen nulo.
-     */
-    public List<Compra> cestasDeLasPersonas(List<Persona> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-    
-    /**
-     * Devuelve las edades de las personas.
-     * No hace falta verificar si valen nulo.
-     */
-    public int[] edadesDeLasPersonas(List<Persona> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }      
-    
-    /**
-     * Devuelve cuantas personas hay en cada ciudad. 
-     * Ciudad contiene dos campos Nombre de la ciudad y cuantas personas hay de la lista.
-     * No hace falta verificar si valen nulo.
-     */
-    public List<Ciudad> cuantasPersonasHayPorCiudad(List<Persona> lista){
+    public List<Persona> top3Personas(List<Persona> lista) {
+        
+      /*  lista.stream()
+                .sorted((x,y)->{
+                    if(x.getCesta.isPresent)
+                    
+                })
+        */
         throw new RuntimeException("Pendiente de hacer");
     }
 
     /**
-     * Top 3 clientes. 
-     * Devuelve los tres clientes que más articulos en la cesta tienen.
-     * No hace falta verificar si valen nulo.
+     * Top 3 ciudades. Devuelve las tres ciudades con más personas en un Set. No
+     * hace falta verificar si valen nulo.
      */
-    public List<Persona> top3Personas(List<Persona> lista){
+    public Set<String> top3Ciudades(List<Persona> lista) {
+        
         throw new RuntimeException("Pendiente de hacer");
-    }    
-    
-    /**
-     * Top 3 ciudades. 
-     * Devuelve las tres ciudades con más personas en un Set.
-     * No hace falta verificar si valen nulo.
-     */
-    public Set<String> top3Ciudades(List<Persona> lista){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
+    }
 
     /**
-     * Devuelve una lista con 3 objetos RangoEdad.
-     * Posicion 0-Cuantas personas hay menores de 18 años.
-     * Posicion 1-Cuantas personas hay entre 18 y 60 años.
-     * Posicion 2-Cuantas personas hay mayores de 60 años.
-     * No hace falta verificar si valen nulo.
+     * Devuelve una lista con 3 objetos RangoEdad. Posicion 0-Cuantas personas
+     * hay menores de 18 años. Posicion 1-Cuantas personas hay entre 18 y 60
+     * años. Posicion 2-Cuantas personas hay mayores de 60 años. No hace falta
+     * verificar si valen nulo.
      */
-    public List<RangoEdad> clasificacionPorRangoDeEdad(List<Persona> lista){
+    public List<RangoEdad> clasificacionPorRangoDeEdad(List<Persona> lista) {
         throw new RuntimeException("Pendiente de hacer");
-    }    
+    }
 
     /**
-     * Devuelve cuantas personas mayores de edad hay en cada ciudad.
-     * Si una ciudad no tiene personas mayores de edad no hace falta devolver ese dato.
-     * Devolver un mapa donde la clave sería la ciudad y el número el número de personas.
-     * No hace falta verificar si valen nulo.
+     * Devuelve cuantas personas mayores de edad hay en cada ciudad. Si una
+     * ciudad no tiene personas mayores de edad no hace falta devolver ese dato.
+     * Devolver un mapa donde la clave sería la ciudad y el número el número de
+     * personas. No hace falta verificar si valen nulo.
      */
-    public Map<String, Integer> cuantasPersonasMayoresDeEdadPorCiudad(List<Persona> lista){
+    public Map<String, Integer> cuantasPersonasMayoresDeEdadPorCiudad(List<Persona> lista) {
         throw new RuntimeException("Pendiente de hacer");
-    }     
-    
+    }
+
 }
--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio09VariadoCapas.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio09VariadoCapas.java
@@ -13,9 +13,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
+/*
+ir a buscar la info
+ y tratar la info aqui o llamar a una capa de servicio para que la trate
+en esta capap no recibes las personas hay que llamar a al capa repositorio 
+que si recibe estas personas
+p.e necesito una lista de personas pero no la tengo tengo que llamar al intrfaz
+que tiene esa lista y que nos devuelve esa lista que contiene la base de datos
+LLAMAR A LOS METODOS QUE LO HACEN
+ */
 public class Ejercicio09VariadoCapas {
-    
+
     private IPersonasRepository repoPersonas;
     private ICiudadesRepository repoCiudades;
     private IComprasRepository repoCompras;
@@ -27,63 +35,70 @@ public class Ejercicio09VariadoCapas {
         this.repoCompras = repoCompras;
         this.serAduana = serAduana;
     }
-    
+
     /**
-     * Busca las personas en el repository que hayan nacido entre dos fecha introducidas y devuelve las ciudades donde están estas personas.
-     * Incluir el número de personas que hay en cada ciudad
-     * Utilizar StreamsParalelos para optimizar el rendimiento de servidores si es posible.
-     * Las ciudades no se deben repetir.
-     * Devolver la información ordenada por el número de personas de mayor a menor.
+     * Busca las personas en el repository que hayan nacido entre dos fecha
+     * introducidas y devuelve las ciudades donde están estas personas. Incluir
+     * el número de personas que hay en cada ciudad Utilizar StreamsParalelos
+     * para optimizar el rendimiento de servidores si es posible. Las ciudades
+     * no se deben repetir. Devolver la información ordenada por el número de
+     * personas de mayor a menor.
      */
-    public List<Ciudad> buscarCiudadesDePersonasNacidasEntreFechas(Date fechaDesde, Date fechaHasta){
+    public List<Ciudad> buscarCiudadesDePersonasNacidasEntreFechas(Date fechaDesde, Date fechaHasta) {
         throw new RuntimeException("Pendiente de hacer");
     }
-    
-           
-    
+
     /**
      * Calcular el total de productos comprados por las personas de una ciudad.
-     * Será necesario leer personas utilizando IPersonasRepository y las compras utilizando IComprasRepository 
-     * Utilizar StreamsParalelos para optimizar el rendimiento de servidores si es posible.
+     * Será necesario leer personas utilizando IPersonasRepository y las compras
+     * utilizando IComprasRepository Utilizar StreamsParalelos para optimizar el
+     * rendimiento de servidores si es posible.
      */
-    public Long calcularComprasDeUnaCiudad(String ciudad){
-        throw new RuntimeException("Pendiente de hacer");
-    }    
-    
-    /**
-     * Calcular el total de productos comprados por las personas de una ciudad entre dos fechas.
-     * Será necesario leer personas utilizando IPersonasRepository y las compras utilizando IComprasRepository 
-     * Las compras se pueden obtener utilizando 
-     * Utilizar StreamsParalelos para optimizar el rendimiento de servidores si es posible.
-     */
-    public Long calcularComprasDeUnaCiudadEntreFechas(String ciudad, Date fechaDesde, Date fechaHasta){
+    public Long calcularComprasDeUnaCiudad(String ciudad) {
         throw new RuntimeException("Pendiente de hacer");
     }
-    
+
     /**
-     * Nos envian una lista de compras realizadas y debemos calcular el importe pasado por la aduana.
-     * Para el cálculo del precio utilizar el servicio IAduanaService.
+     * Calcular el total de productos comprados por las personas de una ciudad
+     * entre dos fechas. Será necesario leer personas utilizando
+     * IPersonasRepository y las compras utilizando IComprasRepository Las
+     * compras se pueden obtener utilizando Utilizar StreamsParalelos para
+     * optimizar el rendimiento de servidores si es posible.
      */
-    public Double calcularImpuestoPorAduanaCompras(List<Compra> compras){
+    public Long calcularComprasDeUnaCiudadEntreFechas(String ciudad, Date fechaDesde, Date fechaHasta) {
         throw new RuntimeException("Pendiente de hacer");
-    }    
-    
+    }
+
     /**
-     * Recibimos una lista de personas y deseamos calcular el total ingresado por aduana obtenido entre dos fechas.
-     * Estas personsa no tienen la compra informada por lo que la tendremos que obtener utilizando IComprasRepository
-     * Para el cálculo del precio utilizar el servicio IAduanaService.
+     * Nos envian una lista de compras realizadas y debemos calcular el importe
+     * pasado por la aduana. Para el cálculo del precio utilizar el servicio
+     * IAduanaService.
      */
-    public Double calcularImpuestoPorAduanaDeComprasDeListaDePersonasEntreFechas(List<Persona> personas, Date fechaDesde, Date fechaHasta){
+    public Double calcularImpuestoPorAduanaCompras(List<Compra> compras) {
         throw new RuntimeException("Pendiente de hacer");
-    } 
-    
+    }
+
     /**
-     * Recibimos una lista de personas que tienen cestas de compra con detalle de producto.
-     * Se pide devolver cuantos ingresos a traves de la aduana hemos obtenido por productos hay de cada tipo de producto y devolver la información ordenada de mayor a menos. (El tipo más vendido al menos)
-     * Utilizaremos el servicio de la aduana para obtener 
-     * Para ello devolveremos una tupla (Pair en el que el primer valor sea el nombre del tipos y el segundo sea cuantas veces aparece).
+     * Recibimos una lista de personas y deseamos calcular el total ingresado
+     * por aduana obtenido entre dos fechas. Estas personsa no tienen la compra
+     * informada por lo que la tendremos que obtener utilizando
+     * IComprasRepository Para el cálculo del precio utilizar el servicio
+     * IAduanaService.
      */
-    public List<Pair<String,Long>> calcularImpuestoPorTipoSegunAduanaDeComprasDeListaDePersonasEntreFechas(List<Persona> personas, Date fechaDesde, Date fechaHasta){
+    public Double calcularImpuestoPorAduanaDeComprasDeListaDePersonasEntreFechas(List<Persona> personas, Date fechaDesde, Date fechaHasta) {
+        throw new RuntimeException("Pendiente de hacer");
+    }
+
+    /**
+     * Recibimos una lista de personas que tienen cestas de compra con detalle
+     * de producto. Se pide devolver cuantos ingresos a traves de la aduana
+     * hemos obtenido por productos hay de cada tipo de producto y devolver la
+     * información ordenada de mayor a menos. (El tipo más vendido al menos)
+     * Utilizaremos el servicio de la aduana para obtener Para ello devolveremos
+     * una tupla (Pair en el que el primer valor sea el nombre del tipos y el
+     * segundo sea cuantas veces aparece).
+     */
+    public List<Pair<String, Long>> calcularImpuestoPorTipoSegunAduanaDeComprasDeListaDePersonasEntreFechas(List<Persona> personas, Date fechaDesde, Date fechaHasta) {
         throw new RuntimeException("Pendiente de hacer");
     }
 
