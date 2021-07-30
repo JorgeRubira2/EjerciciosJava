fdaa4eb384ddbe299dfb8fb7e935a990944943b5
Ignacio <Ignacio@DESKTOP-MSUFQP8>
2021-07-30 13:07:06 +0200

    Colecciones

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio06Streams.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio06Streams.java
@@ -26,7 +26,7 @@ public class Ejercicio06Streams {
      */
     public int maximoElemento(List<Integer> lista){
        int max = 0; 
-    	//max = lista.stream().map(lista);
+    	
     	
        return max;
     }
