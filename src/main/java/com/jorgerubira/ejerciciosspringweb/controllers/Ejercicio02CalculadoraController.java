f8beaf3928a07586d7b6a7996bfd2c7f6e810ede
Mohamad <Mohamad@LAPTOP-MOHA>
2021-08-05 08:50:54 +0200

    Ejercicio HTML

---
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -0,0 +1,10 @@
+package com.jorgerubira.ejerciciosspringweb.controllers;
+
+/**
+ * Crear una calculadora web que ejecute el servicio CalculadoraServices encargada de 
+ * sumar, restar, multiplicar y dividir dos números.
+ * El resultado se mostrará en la parte inferior.
+ */
+public class Ejercicio02CalculadoraController {
+    
+}
