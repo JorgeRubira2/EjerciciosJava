013a6ee728aeb35ec7d5ff0036362c9ea4ce01c1
Mohamad <Mohamad@LAPTOP-MOHA>
2021-08-06 01:50:27 +0200

    Ejercicio Nombres y Calculadora hechos

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
@@ -1,11 +1,52 @@
-
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio01ListaNombresService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
- * Disponemos un servicio IEjercicio01ListaNombresService ya implementado que permite almacenar y borrar nombres de personas.
- * Se pide exponerlo en una web de manera que podremos ver las personas que están en la lista y añadir o quitar nombres.
- * Los nombres no se deben repetir, el servicio lanza una excepción en ese caso.
+ * Disponemos un servicio IEjercicio01ListaNombresService ya implementado que
+ * permite almacenar y borrar nombres de personas. Se pide exponerlo en una web
+ * de manera que podremos ver las personas que están en la lista y añadir o
+ * quitar nombres. Los nombres no se deben repetir, el servicio lanza una
+ * excepción en ese caso.
  */
+@Controller
+
 public class Ejercicio01ListaNombresController {
+
+    @Autowired
+    private IEjercicio01ListaNombresService service;
+
+    @GetMapping("/ejercicio1/listaPersona")
+    public String ListaNombre(Model model) {
+        model.addAttribute("listaPersonas", service.getLista());
+        return "ej01/listaPersonas";
+    }
     
+    @PostMapping("/ejercicio1/AddNombre")
+    public String AddNombre(Model model,String nombre) {
+        try {
+            service.altaNombre(nombre); 
+        } catch (OperacionEnListaException ex) {
+            model.addAttribute("error","No se ha podido añadir el nombre a la lista");
+        }
+         model.addAttribute("nombre", nombre);
+         model.addAttribute("listaPersonas",service.getLista());
+        return "ej01/listaPersonas";
+    }
+    
+    @PostMapping("/ejercicio1/EliminarNombre")
+    public String EliminarNombre(Model model,String nombre) {
+            service.bajaNombre(nombre);
+            model.addAttribute("listaPersonas",service.getLista());
+        return "ej01/listaPersonas";
+    }
+
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -1,11 +1,89 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio02CalculadoraService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * Crear una calculadora web que ejecute el servicio con la interface 
- * IEjercicio02CalculadoraService encargada de 
- * sumar, restar, multiplicar y dividir dos números.
- * El resultado se mostrará en la parte inferior.
+ * Crear una calculadora web que ejecute el servicio con la interface
+ * IEjercicio02CalculadoraService encargada de sumar, restar, multiplicar y
+ * dividir dos números. El resultado se mostrará en la parte inferior.
  */
+@Controller
+@RequestMapping("/ejercicio2")
 public class Ejercicio02CalculadoraController {
-    
+
+    @Autowired
+    private IEjercicio02CalculadoraService service;
+
+    @GetMapping("/calculadora")
+    public String calc(Model model) {
+        model.addAttribute("resultado", 0);
+        model.addAttribute("valor1", 0);
+        model.addAttribute("valor2", 0);
+        return "ej02/calculadoraV2";
+    }
+
+    @PostMapping("/sumar")
+    public String sumar(Model model, Integer valor1, Integer valor2) {
+        if (valor1 == null) {
+            valor1 = 0;
+        }
+        if (valor2 == null) {
+            valor2 = 0;
+        }
+        model.addAttribute("resultado", service.sumar(valor1, valor2));
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadoraV2";
+    }
+
+    @PostMapping("/restar")
+    public String resta(Model model, Integer valor1, Integer valor2) {
+        if (valor1 == null) {
+            valor1 = 0;
+        }
+        if (valor2 == null) {
+            valor2 = 0;
+        }
+        model.addAttribute("resultado", service.restar(valor1, valor2));
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadoraV2";
+    }
+
+    @PostMapping("/producto")
+    public String producto(Model model, Integer valor1, Integer valor2) {
+        if (valor1 == null) {
+            valor1 = 0;
+        }
+        if (valor2 == null) {
+            valor2 = 0;
+        }
+        model.addAttribute("resultado", service.multiplicar(valor1, valor2));
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadoraV2";
+    }
+
+    @PostMapping("/division")
+    public String division(Model model, Integer valor1, Integer valor2) {
+        if (valor1 == null) {
+            valor1 = 0;
+        }
+        if (valor2 == null) {
+            valor2 = 0;
+        }
+        model.addAttribute("resultado", service.dividir(valor1, valor2));
+        if (service.dividir(valor2, valor2) == null) {
+            model.addAttribute("error", "No se puede dividir por cero");
+        }
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadoraV2";
+    }
 }
