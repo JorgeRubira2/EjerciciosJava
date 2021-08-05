2bcce9f1cc473b0a0f6d3cbbc9f4eb2c22bd1717
Jose <martosmartinjose@gmail.com>
2021-08-05 14:36:02 +0200

    ejer01 y 02 spring reealizados

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
@@ -1,11 +1,46 @@
 
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio01ListaNombresService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Disponemos un servicio IEjercicio01ListaNombresService ya implementado que permite almacenar y borrar nombres de personas.
  * Se pide exponerlo en una web de manera que podremos ver las personas que están en la lista y añadir o quitar nombres.
  * Los nombres no se deben repetir, el servicio lanza una excepción en ese caso.
  */
+@Controller
+@RequestMapping("/ej01")
 public class Ejercicio01ListaNombresController {
+    @Autowired
+    private IEjercicio01ListaNombresService listaNombres;
     
+    @GetMapping("/listaPersonas")
+    public String lista(){   
+        return "ej01/listaPersonas";
+    }
+    
+    @PostMapping("/listaPersonas")
+    public String listaInserta(Model m, String nombre, String accion){
+        nombre = nombre.trim();
+        try{
+            if(nombre!=null && !nombre.isEmpty()){
+                if(accion.equals("add")){
+                    listaNombres.altaNombre(nombre); 
+                }else if (accion.equals("del")){
+                    listaNombres.bajaNombre(nombre);
+                }
+            }
+        }catch(OperacionEnListaException e){
+            m.addAttribute("error",true);
+        }
+        m.addAttribute("listaNombres",listaNombres.getLista());
+        return "ej01/listaPersonas";
+    }
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -1,11 +1,56 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio02CalculadoraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Crear una calculadora web que ejecute el servicio con la interface 
  * IEjercicio02CalculadoraService encargada de 
  * sumar, restar, multiplicar y dividir dos números.
  * El resultado se mostrará en la parte inferior.
  */
+@Controller
+@RequestMapping("/ej02")
 public class Ejercicio02CalculadoraController {
+    @Autowired
+    private IEjercicio02CalculadoraService calculadora;
     
+    @GetMapping("/calculadora")
+    public String calc(){   
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/calculadora")
+    public String calculo(Model m, Integer num1, Integer num2, String accion){
+        Integer res = 0;
+        if (num1!=null && num2!=null){
+            m.addAttribute("num1",num1);
+            m.addAttribute("num2",num2);
+            switch(accion){
+                case "sumar":
+                    res = calculadora.sumar(num1, num2);
+                    break;
+                case "restar":
+                    res = calculadora.restar(num1, num2);
+                    break;
+                case "mult":
+                    res = calculadora.multiplicar(num1, num2);
+                    break;
+                case "div":
+                    if (num1>0 && num2>0){
+                        res = calculadora.dividir(num1, num2);
+                    }else{
+                        m.addAttribute("error",true);
+                    }
+                    break;
+            }
+            m.addAttribute("resultado",res);
+        }
+        return "ej02/calculadora";
+    }
 }
