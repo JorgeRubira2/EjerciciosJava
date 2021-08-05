0182a149cc4063d7acfb79ae1fd617dc6c34ec4f
JorgeRubira <jorgesub@gmail.com>
2021-08-05 08:16:54 +0200

    Ejercicios Lambda y Streams

---
+++ src/main/java/com/jorgerubira/explicaciones/D20210804/AlgoritmoReflexivo.java
@@ -0,0 +1,42 @@
+package com.jorgerubira.explicaciones.D20210804;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MiAnotacion{
+}
+
+class Clase1{
+    
+    @MiAnotacion
+    public void saludar(){
+        System.out.println("Hola");
+    }
+}
+
+public class AlgoritmoReflexivo {
+    
+    public static void ejecutarMetodos(Clase1 obj) throws Exception{
+        Class cls = obj.getClass();
+        Method[] metodos=cls.getMethods();
+        for (Method met:metodos){
+            if (met.isAnnotationPresent(MiAnotacion.class) ){
+                met.invoke(obj);    
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Clase1 c=new Clase1();
+        try{
+            ejecutarMetodos(c);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
