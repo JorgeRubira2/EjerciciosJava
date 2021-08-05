2d29c57b7cff5466a4ab50d6417f6cea6444a97a
Jochemali <gimenezleon98@gmail.com>
2021-08-05 15:44:48 +0200

    Comenzando con Spring. Ejercicio Calculadora completado

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
@@ -1,11 +1,59 @@
-
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio01ListaNombresService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
- * Disponemos un servicio IEjercicio01ListaNombresService ya implementado que permite almacenar y borrar nombres de personas.
- * Se pide exponerlo en una web de manera que podremos ver las personas que están en la lista y añadir o quitar nombres.
- * Los nombres no se deben repetir, el servicio lanza una excepción en ese caso.
+ * Disponemos un servicio IEjercicio01ListaNombresService ya implementado que
+ * permite almacenar y borrar nombres de personas. Se pide exponerlo en una web
+ * de manera que podremos ver las personas que están en la lista y añadir o
+ * quitar nombres. Los nombres no se deben repetir, el servicio lanza una
+ * excepción en ese caso.
  */
+@Controller
+@RequestMapping("/ejercicio1")
 public class Ejercicio01ListaNombresController {
-    
+
+    @Autowired
+    private IEjercicio01ListaNombresService lista;
+
+    @GetMapping("/lista")
+    public String lista(Model model) {
+        model.addAttribute("lista", lista.getLista());
+        return "ej01/listaPersonas";
+    }
+
+    @PostMapping("/enviar")
+    public String enviarNombre(Model model, String nombre,
+            @RequestParam(value = "Add", required = false) String add,
+            @RequestParam(value = "Borrar", required = false) String borrar) {
+
+        try {
+
+            if (add != null && add.equals("Añadir")) {
+                lista.altaNombre(nombre);
+            } else if (borrar != null && borrar.equals("Borrar")) {
+                lista.bajaNombre(nombre);
+            }
+
+            model.addAttribute("nombre", nombre);
+        } catch (OperacionEnListaException ex) {
+            Logger.getLogger(Ejercicio01ListaNombresController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        model.addAttribute("lista", lista.getLista());
+
+        return "ej01/listaPersonas";
+
+    }
+
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -1,11 +1,63 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio02CalculadoraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Crear una calculadora web que ejecute el servicio con la interface 
  * IEjercicio02CalculadoraService encargada de 
  * sumar, restar, multiplicar y dividir dos números.
  * El resultado se mostrará en la parte inferior.
  */
+@Controller
+@RequestMapping("/ejercicio2")
 public class Ejercicio02CalculadoraController {
+    @Autowired
+    private IEjercicio02CalculadoraService calculadora;
+    
+    @GetMapping("/calculadora")
+    public String calculadora(Model model){
+        return ("ej02/calculadora");
+    }
+    
+    @RequestMapping(value="/resultado", method=RequestMethod.POST, params="operacion=sumar")
+    public String suma(Model model,
+            @RequestParam("valor1") Integer a,
+            @RequestParam("valor2") Integer b){   
+        model.addAttribute("resultado", calculadora.sumar(a, b));
+        
+        return ("ej02/calculadora");
+    }
+    @RequestMapping(value="/resultado", method=RequestMethod.POST, params="operacion=restar")
+    public String resta(Model model,
+            @RequestParam("valor1") Integer a,
+            @RequestParam("valor2") Integer b){   
+        model.addAttribute("resultado", calculadora.restar(a, b));
+        
+        return ("ej02/calculadora");
+    }
+    @RequestMapping(value="/resultado", method=RequestMethod.POST, params="operacion=multiplicar")
+    public String multiplicacion(Model model,
+            @RequestParam("valor1") Integer a,
+            @RequestParam("valor2") Integer b){   
+        model.addAttribute("resultado", calculadora.multiplicar(a, b));
+        
+        return ("ej02/calculadora");
+    }
+    @RequestMapping(value="/resultado", method=RequestMethod.POST, params="operacion=dividir")
+    public String division(Model model,
+            @RequestParam("valor1") Integer a,
+            @RequestParam("valor2") Integer b){   
+        model.addAttribute("resultado", calculadora.dividir(a, b));
+        
+        return ("ej02/calculadora");
+    }
     
 }
