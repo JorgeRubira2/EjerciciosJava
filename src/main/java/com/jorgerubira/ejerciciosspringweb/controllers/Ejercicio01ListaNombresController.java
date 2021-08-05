148c0d4bf8d35dc9d3e553ac1054d6d4cfe39137
Jesus <Jesus@192.168.1.24>
2021-08-05 13:44:11 +0200

    Ejercicio 1 y 2

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
@@ -1,11 +1,60 @@
 
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio01ListaNombresService;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Disponemos un servicio IEjercicio01ListaNombresService ya implementado que permite almacenar y borrar nombres de personas.
  * Se pide exponerlo en una web de manera que podremos ver las personas que están en la lista y añadir o quitar nombres.
  * Los nombres no se deben repetir, el servicio lanza una excepción en ese caso.
  */
+@Controller
+@RequestMapping("/ejercicio01")
 public class Ejercicio01ListaNombresController {
+    @Autowired
+    private IEjercicio01ListaNombresService service;
+    
+    @GetMapping("/listaNombres")
+    public String inicio(Model model){
+        model.addAttribute("listaPersonas", service.getLista());
+        return "ej01/listaPersonas";
+    }
+    @PostMapping("/listaNombres")
+    public String accion(Model model,String nombre, String toDo){
+        List<String> aux=service.getLista();
+        String resultado="";
+        if(toDo.equalsIgnoreCase("añadir")){
+            try {
+                service.altaNombre(nombre);
+                resultado=nombre+" añadido con éxito";
+            } catch (OperacionEnListaException ex) {
+                Logger.getLogger(Ejercicio01ListaNombresController.class.getName()).log(Level.SEVERE, null, ex);
+                resultado=nombre+" error al añadir";
+            }
+        }
+        else if(toDo.equalsIgnoreCase("borrar")){
+            if(aux.contains(nombre)){
+                service.bajaNombre(nombre);
+                resultado=nombre+" borrado correctamente";
+            }
+            else{
+                resultado=nombre+" este elemento no se encontraba en la lista";
+            }
+          
+        }
+        model.addAttribute("resultado", resultado);
+        model.addAttribute("listaPersonas", service.getLista());
+        return "ej01/listaPersonas";
+    }
     
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -1,11 +1,60 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
-/**
- * Crear una calculadora web que ejecute el servicio con la interface 
- * IEjercicio02CalculadoraService encargada de 
- * sumar, restar, multiplicar y dividir dos números.
- * El resultado se mostrará en la parte inferior.
- */
+
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio02CalculadoraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/ejercicio02")
 public class Ejercicio02CalculadoraController {
+    @Autowired
+    private IEjercicio02CalculadoraService service;
+    
+    @GetMapping("/calculadora")
+    public String inicio(){
+        return "ej02/calculadora";
+    }
+    @PostMapping("/calculadora")
+    public String calculadora(Model model, String operacion, Integer valor1, Integer valor2){
+        if (valor1 == null) {
+            valor1 = 0;
+        }
+        if (valor2 == null) {
+            valor2 = 0;
+        }
+        if(operacion.equalsIgnoreCase("Sumar")){
+            model.addAttribute("valor1", valor1);
+            model.addAttribute("valor2", valor2);
+            model.addAttribute("resultado",service.sumar(valor1, valor2));
+        }
+        else if(operacion.equalsIgnoreCase("Restar")){
+            model.addAttribute("valor1", valor1);
+            model.addAttribute("valor2", valor2);
+            model.addAttribute("resultado",service.restar(valor1, valor2));
+        }
+        else if(operacion.equalsIgnoreCase("multiplicar")){
+            model.addAttribute("valor1", valor1);
+            model.addAttribute("valor2", valor2);
+            model.addAttribute("resultado",service.multiplicar(valor1, valor2));
+        }
+        else if(operacion.equalsIgnoreCase("dividir")){           
+            if(valor2==0){
+                valor2=1;
+            }
+            model.addAttribute("valor1", valor1);
+            model.addAttribute("valor2", valor2);
+            model.addAttribute("resultado",service.dividir(valor1, valor2));
+        }
+        
+        
+        return "ej02/calculadora";
+    }
+    
     
 }
