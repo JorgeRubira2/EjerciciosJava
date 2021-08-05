96f7f33ca96b9617213cf9b142cb3cdc4999cd3b
Javier A <javialiaga2000@gmail.com>
2021-08-05 14:31:31 +0200

    Ej2 (sin acabar) Spring

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -3,7 +3,9 @@ package com.jorgerubira.ejerciciosspringweb.controllers;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio02CalculadoraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,62 @@ public class Ejercicio02CalculadoraController {
     
     @GetMapping("/calculadora")
     public String calculadora(){
-        return "/ej02/calculadora";
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/sumar")
+    public String sumar(Model model, Integer valor1, Integer valor2){
+        if (valor1==null){
+            valor1=0;
+        }
+        if (valor2==null){
+            valor2=0;
+        }
+        model.addAttribute("resultado", servicio.sumar(valor1, valor2));    
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/restar")
+    public String restar(Model model, Integer valor1, Integer valor2){
+        if (valor1==null){
+            valor1=0;
+        }
+        if (valor2==null){
+            valor2=0;
+        }
+        model.addAttribute("resultado", servicio.restar(valor1, valor2));    
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/multiplicar")
+    public String multiplicar(Model model, Integer valor1, Integer valor2){
+        if (valor1==null){
+            valor1=0;
+        }
+        if (valor2==null){
+            valor2=0;
+        }
+        model.addAttribute("resultado", servicio.multiplicar(valor1, valor2));    
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/dividir")
+    public String dividir(Model model, Integer valor1, Integer valor2){
+        if (valor1==null){
+            valor1=0;
+        }
+        if (valor2==null){
+            valor2=0;
+        }
+        model.addAttribute("resultado", servicio.dividir(valor1, valor2));    
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        return "ej02/calculadora";
     }
 }
