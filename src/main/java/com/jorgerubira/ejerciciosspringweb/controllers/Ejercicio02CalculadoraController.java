854bc003a6d83f9f4a2a20d5276d4011ea1ab4f1
Isabel <Isabel@LAPTOP-JF6QL3V5.domain.name>
2021-08-06 14:37:24 +0200

    Terminada calculadora

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio02CalculadoraController.java
@@ -1,11 +1,67 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio02CalculadoraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Crear una calculadora web que ejecute el servicio con la interface 
  * IEjercicio02CalculadoraService encargada de 
  * sumar, restar, multiplicar y dividir dos números.
  * El resultado se mostrará en la parte inferior.
  */
+@Controller
+@RequestMapping("/ejercicio02")
 public class Ejercicio02CalculadoraController {
     
+    @Autowired
+    private IEjercicio02CalculadoraService calculadoraService;
+    
+    @PostMapping("/sumar")
+    public String sumar(Model model, Integer valor1, Integer valor2){
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        model.addAttribute("resultado", calculadoraService.sumar(valor1, valor2));
+        
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/restar")
+    public String restar(Model model, Integer valor1, Integer valor2){
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        model.addAttribute("resultado", calculadoraService.restar(valor1, valor2));
+        
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/multiplicar")
+    public String multiplicar(Model model, Integer valor1, Integer valor2){
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        model.addAttribute("resultado", calculadoraService.multiplicar(valor1, valor2));
+        
+        return "ej02/calculadora";
+    }
+    
+    @PostMapping("/dividir")
+    public String dividir(Model model, Integer valor1, Integer valor2){
+        model.addAttribute("valor1", valor1);
+        model.addAttribute("valor2", valor2);
+        model.addAttribute("resultado", calculadoraService.dividir(valor1, valor2));
+        
+        return "ej02/calculadora";
+    }
+    
+    @GetMapping("/resultado")
+    public String resultado(Model model){
+        model.addAttribute("resultado");
+        
+        return "ej02/calculadora";
+    }
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosService.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosService.java
@@ -2,23 +2,40 @@ package com.jorgerubira.ejerciciosspringweb.services;
 
 import com.jorgerubira.ejerciciosspringweb.domain.Alumno;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio03GestionAlumnosService;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 
 /**
  * Servicio que implementa el interface de gestion de alumnos
  */
+@Service
 public class Ejercicio03GestionAlumnosService implements IEjercicio03GestionAlumnosService {
 
+    private List<Alumno> listaAlumnos = new ArrayList();
+    
     @Override
     public void guardarAlumno(Alumno alumno) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        
+        
+        Alumno nuevoAlumno = new Alumno();
+        nuevoAlumno.setCodigo(alumno.getCodigo());
+        nuevoAlumno.setNombre(alumno.getNombre());
+        nuevoAlumno.setTelefono(alumno.getDireccion());
+        nuevoAlumno.setDireccion(alumno.getDireccion());
+        
+     
     }
 
     @Override
     public void borrarAlumno(Long codigo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Alumno alumno = new Alumno();
+        alumno.getCodigo();
+        
+        borrarAlumno(codigo);
     }
 
     @Override
