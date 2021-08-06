049ac701cfd1c87052b714a5c33afd11cabc5ec2
Jesus <Jesus@192.168.1.24>
2021-08-06 16:42:47 +0200

    Ejercicio 03- completo

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio03GestionAlumnosController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio03GestionAlumnosController.java
@@ -24,52 +24,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/ejercicio03")
 public class Ejercicio03GestionAlumnosController {
-    
+
     @Autowired
     private IEjercicio03GestionAlumnosService service;
-    
+
     @GetMapping("/alumnos")
-    public String inicio(Model model, Integer codigo){
-        if(codigo!=null){
+    public String inicio(Model model, Integer codigo) {
+        if (codigo != null) {
             service.borrarAlumno(codigo.longValue());
         }
-        model.addAttribute("lista",service.getAlumnos());
-        model.addAttribute("number", (int)(Math.random()*99999999));
+        model.addAttribute("lista", service.getAlumnos());
+        model.addAttribute("number", (int) (Math.random() * 99999999));//número aleatorio para nuevo alumno
         return "/ej03/listaAlumnos";
     }
+
     @PostMapping("/alumnos")
-    public String modificacionAlumno(Model model, Integer codigo, String nombre, String telefono, String direccion){
-        
-        long a=codigo.longValue();
-        Alumno nuevo=new Alumno(a,nombre,telefono,direccion);
+    public String modificacionAlumno(Model model, Integer codigo, String nombre, String telefono, String direccion) {
+        long a = codigo.longValue();
+        Alumno nuevo = new Alumno(a, nombre, telefono, direccion);
         service.guardarAlumno(nuevo);
-        model.addAttribute("lista",service.getAlumnos());
-        model.addAttribute("number", (int)(Math.random()*99999999));
+        model.addAttribute("lista", service.getAlumnos());
+        model.addAttribute("number", (int) (Math.random() * 99999999));//número aleatorio para nuevo alumno
         return "/ej03/listaAlumnos";
     }
+
     @GetMapping("/alumnosBuscados")
-    public String buscador(Model model, String buscar){
-        List<Alumno>aux=new ArrayList<>();
-       
-            aux=service.getAlumnos(buscar);
-        
-        model.addAttribute("lista",aux);
-        model.addAttribute("number", (int)(Math.random()*99999999));
-        
+    public String buscador(Model model, String buscar) {
+        List<Alumno> aux = new ArrayList<>();
+        aux = service.getAlumnos(buscar);
+        model.addAttribute("lista", aux);
+        model.addAttribute("number", (int) (Math.random() * 99999999));//número aleatorio para nuevo alumno
         return "/ej03/listaAlumnos";
     }
-    
+
     @GetMapping("/alumnosModificar")
-    public String modificador(Model model, Integer codigo){
-        Optional<Alumno>aux;
-        long a=codigo.longValue();
-        aux=service.getAlumno(a);
-        Alumno resultado=aux.get();
-        
-        model.addAttribute("alumno",resultado);
+    public String modificador(Model model, Integer codigo) {
+        Optional<Alumno> aux;
+        long a = codigo.longValue();
+        aux = service.getAlumno(a);
+        Alumno resultado = aux.get();
+        model.addAttribute("alumno", resultado);
         return "/ej03/modificarAlumno";
     }
-    
-   
-    
+
 }
--- src/test/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosServiceTest.java
+++ src/test/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosServiceTest.java
@@ -17,42 +17,41 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author PC
  */
 public class Ejercicio03GestionAlumnosServiceTest {
-    
+
     public Ejercicio03GestionAlumnosServiceTest() {
     }
 
     @Test
     public void testGuardarAlumno() {
         Ejercicio03GestionAlumnosService instance = new Ejercicio03GestionAlumnosService();
-        long codigo=(int)(Math.random()*99999999);
+        long codigo = (int) (Math.random() * 99999999);
         Alumno alumno = new Alumno(codigo, "A", "A", "A");
-        int size=instance.getAlumnos().size();
+        int size = instance.getAlumnos().size();
         instance.guardarAlumno(alumno);
-        assertEquals(size+1, instance.getAlumnos().size());
+        assertEquals(size + 1, instance.getAlumnos().size());
         Alumno alumno2 = new Alumno(codigo, "B", "B", "B");
         instance.guardarAlumno(alumno2);
-        assertEquals(0, instance.getAlumnos().stream().filter(x->x.getCodigo()==codigo && "A".equals(x.getNombre())).count());
-        assertEquals(0, instance.getAlumnos().stream().filter(x->x.getCodigo()==codigo && "A".equals(x.getDireccion())).count());
-        assertEquals(0, instance.getAlumnos().stream().filter(x->x.getCodigo()==codigo && "A".equals(x.getTelefono())).count());
-        assertEquals(1, instance.getAlumnos().stream().filter(x->x.getCodigo()==codigo && "B".equals(x.getNombre())).count());
-        assertEquals(1, instance.getAlumnos().stream().filter(x->x.getCodigo()==codigo && "B".equals(x.getDireccion())).count());
-        assertEquals(1, instance.getAlumnos().stream().filter(x->x.getCodigo()==codigo && "B".equals(x.getTelefono())).count());
-        
-        
+        assertEquals(0, instance.getAlumnos().stream().filter(x -> x.getCodigo() == codigo && "A".equals(x.getNombre())).count());
+        assertEquals(0, instance.getAlumnos().stream().filter(x -> x.getCodigo() == codigo && "A".equals(x.getDireccion())).count());
+        assertEquals(0, instance.getAlumnos().stream().filter(x -> x.getCodigo() == codigo && "A".equals(x.getTelefono())).count());
+        assertEquals(1, instance.getAlumnos().stream().filter(x -> x.getCodigo() == codigo && "B".equals(x.getNombre())).count());
+        assertEquals(1, instance.getAlumnos().stream().filter(x -> x.getCodigo() == codigo && "B".equals(x.getDireccion())).count());
+        assertEquals(1, instance.getAlumnos().stream().filter(x -> x.getCodigo() == codigo && "B".equals(x.getTelefono())).count());
+
     }
 
     @Test
     public void testBorrarAlumno() {
         System.out.println("borrarAlumno");
-        long codigo=(int)(Math.random()*99999999);
+        long codigo = (int) (Math.random() * 99999999);
         Ejercicio03GestionAlumnosService instance = new Ejercicio03GestionAlumnosService();
         Alumno alumno = new Alumno(codigo, "A", "A", "A");
         instance.guardarAlumno(alumno);
-        int size=instance.getAlumnos().size();
+        int size = instance.getAlumnos().size();
         instance.borrarAlumno(codigo);
-        assertEquals(size-1, instance.getAlumnos().size());
+        assertEquals(size - 1, instance.getAlumnos().size());
         instance.borrarAlumno(codigo); //Si no lo encuentra no lo borra
-        assertEquals(size-1, instance.getAlumnos().size());
+        assertEquals(size - 1, instance.getAlumnos().size());
     }
 
     @Test
@@ -64,9 +63,9 @@ public class Ejercicio03GestionAlumnosServiceTest {
     @Test
     public void testGetAlumno() {
         Ejercicio03GestionAlumnosService instance = new Ejercicio03GestionAlumnosService();
-        long codigo1=(int)(Math.random()*99999999);
-        long codigo2=(int)(Math.random()*99999999);
-        Alumno a=new Alumno(codigo1,"A","A","A");
+        long codigo1 = (int) (Math.random() * 99999999);
+        long codigo2 = (int) (Math.random() * 99999999);
+        Alumno a = new Alumno(codigo1, "A", "A", "A");
         instance.guardarAlumno(a);
         assertEquals(true, instance.getAlumno(codigo1).isPresent());
         assertEquals(a, instance.getAlumno(codigo1).get());
@@ -75,14 +74,14 @@ public class Ejercicio03GestionAlumnosServiceTest {
 
     @Test
     public void testGetAlumnos_String() {
-        String nombre=UUID.randomUUID().toString();
-        Alumno a1=new Alumno((int)(Math.random()*99999999),nombre,"A","A");
-        Alumno a2=new Alumno((int)(Math.random()*99999999),nombre,"A","A");
+        String nombre = UUID.randomUUID().toString();
+        Alumno a1 = new Alumno((int) (Math.random() * 99999999), nombre, "A", "A");
+        Alumno a2 = new Alumno((int) (Math.random() * 99999999), nombre, "A", "A");
         Ejercicio03GestionAlumnosService instance = new Ejercicio03GestionAlumnosService();
         instance.guardarAlumno(a1);
         instance.guardarAlumno(a2);
         assertEquals(2, instance.getAlumnos(nombre).stream().count());
-        
+
     }
-    
+
 }
