54f5927edac6ee45453acfd978f5e4bd4e031ff1
Fran <Fran@DESKTOP-SFBDL1E>
2021-08-06 14:19:11 +0200

    06/08/2021

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio01ListaNombresController.java
@@ -3,9 +3,12 @@ package com.jorgerubira.ejerciciosspringweb.controllers;
 
 import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio01ListaNombresService;
+import com.sun.istack.logging.Logger;
+import java.util.logging.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,21 +22,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class Ejercicio01ListaNombresController {
     
-    
     @Autowired
     private IEjercicio01ListaNombresService service;
     
-        
-    @PostMapping("/listaPersona")
-    public String agregarPersona(Model model, String nombre){
-        
-        try{
-            service.altaNombre(nombre);
-        }catch (OperacionEnListaException ex){
-            model.addAttribute("error", "ha ocurrido un error");
-            
-        }
-        return "ej01/listaPersonas";
-            
+    @GetMapping("/listaPersonas")
+       public String listaPersonas(Model model, String nombre){
+           
+              model.addAttribute("persona",service.getLista());
+              return "ej01/listaPersonas";
     }
+       
+    @PostMapping("/agregar")
+       public String agregar(Model model, String nombre){
+           
+           
+              try{
+                  service.altaNombre(nombre);
+              }catch(OperacionEnListaException ex){
+                 // Logger.getLogger(Ejercicio01ListaNombresController.class.getName()).log(Level.SEVERE, null, ex);
+              }
+              
+              model.addAttribute("persona",service.getLista());
+              
+              return "ej01/listaPersonas";
+    }     
+       
+    @PostMapping("/eliminar")
+       public String eliminar(Model model, String nombre){
+           
+               lista.bajaNombre(nombre);
+               model.addAttribute("Lista", lista.getLista());
+           
+           
+              return "ej01/listaPersonas";
+    }            
+       
+       
 }
---
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/lista.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.ejerciciosspringweb.controllers;
+
+/**
+ *
+ * @author Fran
+ */
+class lista {
+
+    static void bajaNombre(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getLista() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
