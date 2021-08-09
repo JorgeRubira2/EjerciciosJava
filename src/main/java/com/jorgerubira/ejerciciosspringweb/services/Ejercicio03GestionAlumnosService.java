a12addf686c72a14c24d4c92be1e744be3580d37
Mohamad <Mohamad@LAPTOP-MOHA>
2021-08-09 15:08:52 +0200

    Update diario

--- src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosService.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosService.java
@@ -13,28 +13,29 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class Ejercicio03GestionAlumnosService implements IEjercicio03GestionAlumnosService {
-
+    
     private List<Alumno> alumnos = new ArrayList<>();
     
     @Override
-    public void guardarAlumno(Alumno alumno) {
-        for (int i = 0; i < alumnos.size(); i++) {
-            if (alumnos.get(i).getCodigo() == alumno.getCodigo()) {
-                alumnos.set(i, alumno);
-            } else {
-                alumnos.add(alumno);
-            }
-            
+    public void guardarAlumno(Alumno alumno) {        
+        Alumno alu = alumnos.stream()
+                .filter(x -> x.getCodigo() == alumno.getCodigo())
+                .findFirst()
+                .orElse(null);        
+        if (alu == null) {
+            alumnos.add(alumno);
+        } else {
+            alu.setCodigo(alumno.getCodigo());
+            alu.setNombre(alumno.getNombre());
+            alu.setTelefono(alumno.getTelefono());
+            alu.setDireccion(alumno.getDireccion());
         }
     }
     
     @Override
     public void borrarAlumno(Long codigo) {
-        for (int i = 0; i < alumnos.size(); i++) {
-            if (alumnos.get(i).getCodigo() == codigo) {
-                alumnos.remove(i);
-            }
-        }
+        alumnos.removeIf(x -> x.getCodigo() == codigo);
+        
     }
     
     @Override
@@ -48,7 +49,7 @@ public class Ejercicio03GestionAlumnosService implements IEjercicio03GestionAlum
                 .filter(x -> x.getCodigo() == codigo)
                 .findFirst();
         
-        return opt;        
+        return opt;
     }
     
     @Override
