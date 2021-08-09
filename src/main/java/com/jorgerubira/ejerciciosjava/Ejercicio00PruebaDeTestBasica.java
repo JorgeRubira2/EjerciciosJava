2bc97d5efbfb1aeb710381ede236c93d50443d00
Sofia Fiumicino <>
2021-08-09 12:55:37 +0200

    Merge origin/master into Sofia_F
    Conflicts:
    	src/main/resources/templates/ej01/listaPersonas.html

--- src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java
+++ src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java
@@ -4,14 +4,20 @@ package com.jorgerubira.ejerciciosjava;
 public class Ejercicio00PruebaDeTestBasica {
     
     public int sumar(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+        
+        return a+b;
+ 
+        //throw new RuntimeException("Pendiente de hacer");
     }
 
     public int restar(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+    
+        return a-b;
+        //throw new RuntimeException("Pendiente de hacer");
     }
 
     public int multiplicar(int a, int b){
-        throw new RuntimeException("Pendiente de hacer");
+        return a*b;
+       // throw new RuntimeException("Pendiente de hacer");
     }
 }
---
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/EjemploTemplate.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.ejerciciosspringweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Christian
+ */
+@Controller
+@RequestMapping ("/ejemplotemplate")
+
+public class EjemploTemplate {
+    @GetMapping("/texto1")
+    
+    public String text1(){
+            return "/ejte/vista1";
+     
+    }
+    
+    @GetMapping("/texto2")
+    public String text2(){
+            return "/ejte/vista2";
+       
+    }
+}
