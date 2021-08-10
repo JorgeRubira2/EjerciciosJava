80eb3bf55ad8bf14d24c4367802b6eed7aa38269
Jose <martosmartinjose@gmail.com>
2021-08-10 14:37:20 +0200

    Ejer05 en progreso
    Merge origin/master into jose
    
    Conflicts:
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio01ConversionTipos.java
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio03Comparadores.java
    	src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosService.java

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio05MedalleroController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio05MedalleroController.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
-/**
- *
- * @author PC
- */
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio05MedalleroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ej05")
 public class Ejercicio05MedalleroController {
+
+    @Autowired
+    private IEjercicio05MedalleroService servicio;
     
+    @GetMapping("/medallas")
+    public String inicio(Model m){
+        m.addAttribute("medallas",servicio.obtenerRankingPorPais());
+        return "ej05/medallero";
+    }
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio05MedalleroService.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio05MedalleroService.java
@@ -1,18 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jorgerubira.ejerciciosspringweb.services;
 
 import com.jorgerubira.ejerciciosspringweb.domain.Medalla;
+import com.jorgerubira.ejerciciosspringweb.domain.MedallaAtleta;
+import com.jorgerubira.ejerciciosspringweb.domain.MedallaPais;
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio05MedalleroService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
-/**
- *
- */
-public class Ejercicio05MedalleroService {
+@Service
+public class Ejercicio05MedalleroService implements IEjercicio05MedalleroService{
     
     private List<Medalla> medallas = new ArrayList<>();
     
@@ -29,5 +27,63 @@ public class Ejercicio05MedalleroService {
         medallas.add(new Medalla("China", "Clavados masculino", "Plata", "Natacion", "J. Yang"));
         medallas.add(new Medalla("Gran Bretaña", "Clavados masculino", "Cobre", "Natacion", "T. Daley"));
     }
+
+    @Override
+    public void altaMedalla(Medalla medalla) {
+        medallas.add(medalla);
+    }
+
+    @Override
+    public List<Medalla> getMedallas() {
+        return medallas;
+    }
+
+    @Override
+    public List<MedallaPais> obtenerRankingPorPais() { //habria que pasar pais?
+        return medallas.stream()
+                .collect(Collectors.groupingBy((x)-> x.getPais(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted((x,y) -> y.getValue().intValue()-x.getValue().intValue())
+                .map(x-> new MedallaPais(
+                            x.getKey(),
+                            (int)medallas.stream().filter(z->z.getPais().equals(x.getKey()) && "Oro".equals(z.getMedalla())).count(),
+                            (int)medallas.stream().filter(z->z.getPais().equals(x.getKey()) && "Plata".equals(z.getMedalla())).count(),
+                            (int)medallas.stream().filter(z->z.getPais().equals(x.getKey()) && "Cobre".equals(z.getMedalla())).count()
+                )).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> obtenerDeportesDeUnaMedalla(String pais, String medalla) {
+        return medallas.parallelStream()
+                        .filter(x-> pais.equalsIgnoreCase(x.getPais()))
+                        .filter(x-> medalla.equalsIgnoreCase(x.getMedalla()))
+                        .map(x-> x.getDeporte())
+                        .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> obtenerDeportesConMedalla(String pais) {
+        return medallas.parallelStream()
+                        .filter(x-> pais.equalsIgnoreCase(x.getPais()))
+                        .map(x-> x.getDeporte())
+                        .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<MedallaAtleta> obtenerRankingPorAlteta() {
+        return medallas.stream()
+                .collect(Collectors.groupingBy((x)-> x.getDeportistas(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted((x,y) -> y.getValue().intValue()-x.getValue().intValue())
+                .map(x-> new MedallaAtleta(
+                            x.getKey(),
+                            (int)medallas.stream().filter(z->z.getPais().equals(x.getKey()) && "Oro".equals(z.getMedalla())).count(),
+                            (int)medallas.stream().filter(z->z.getPais().equals(x.getKey()) && "Plata".equals(z.getMedalla())).count(),
+                            (int)medallas.stream().filter(z->z.getPais().equals(x.getKey()) && "Cobre".equals(z.getMedalla())).count()
+                )).collect(Collectors.toList());
+    }
+    
     
 }
