b42e0c902efad576fc205462a4ba2385ccd4118a
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-08-11 13:27:53 +0200

    ej3 completo

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio03GestionAlumnosController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio03GestionAlumnosController.java
@@ -5,17 +5,20 @@
  */
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jorgerubira.ejerciciosspringweb.domain.Alumno;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio03GestionAlumnosService;
 
-/**   
+/**
  *
  * @author PC
  */
@@ -31,34 +34,42 @@ public class Ejercicio03GestionAlumnosController {
 		model.addAttribute("lista", service.getAlumnos());
 		return "ej03/gestionAlumno";
 	}
-	 @PostMapping("/gestionAlumno")
-	    public String nuevo(Model model, Long codigo, String nombre, String telefono, String direccion) {
-	        Alumno alumnos = new Alumno();
-	        alumnos.setCodigo(codigo);
-	        alumnos.setNombre(nombre);
-	        alumnos.setTelefono(telefono);
-	        alumnos.setDireccion(direccion);
-	        service.guardarAlumno(alumnos);
-	        model.addAttribute("lista", service.getAlumnos());
 
-	        return "ej03/gestionAlumno";
-	    }
+	@PostMapping("/gestionAlumno")
+	public String nuevo(Model model, Long codigo, String nombre, String telefono, String direccion) {
+		Alumno alumnos = new Alumno();
+		alumnos.setCodigo(codigo);
+		alumnos.setNombre(nombre);
+		alumnos.setTelefono(telefono);
+		alumnos.setDireccion(direccion);
+		service.guardarAlumno(alumnos);
+		model.addAttribute("lista", service.getAlumnos());
 
-	    @GetMapping("/gestionAlumno/eliminar")
-	    public String eliminarAlumnos(Model model, Long codigo) {
+		return "ej03/gestionAlumno";
+	}
 
-	        service.borrarAlumno(codigo);
-	        model.addAttribute("lista", service.getAlumnos());
+	@GetMapping("/gestionAlumno/eliminar")
+	public String eliminarAlumnos(Model model, Long codigo) {
 
-	        return "ej03/gestionAlumno";
-	    }
-	    @PostMapping("/buscarAlumno")
-	    public String buscarAlumnos(Model model, String nombre) {
-	            
-	        
-	    model.addAttribute("lista", service.getAlumnos(nombre));
-	  
-	        return "ej03/gestionAlumno";
-	    }
+		service.borrarAlumno(codigo);
+		model.addAttribute("lista", service.getAlumnos());
+
+		return "ej03/gestionAlumno";
+	}
+
+	@PostMapping("/buscarAlumno")
+	public String buscarAlumnos(Model model, String nombre) {
+
+		model.addAttribute("lista", service.getAlumnos(nombre));
+
+		return "ej03/gestionAlumno";
+	}
+
+	@GetMapping("/gestionAlumno/modificar")
+	@ResponseBody
+	public Alumno editarAlumnos(Long codigo) {
+
+		return service.getAlumno(codigo).get();
+	}
 
 }
