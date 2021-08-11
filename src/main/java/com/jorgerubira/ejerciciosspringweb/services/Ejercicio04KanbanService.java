e490d221457867e2c8ce4f337ab09321431432c4
Isabel <Isabel@LAPTOP-JF6QL3V5.domain.name>
2021-08-11 14:31:39 +0200

    Comenzado Kanban

--- src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio04KanbanService.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio04KanbanService.java
@@ -3,23 +3,50 @@ package com.jorgerubira.ejerciciosspringweb.services;
 import com.jorgerubira.ejerciciosspringweb.domain.TareaKanban;
 import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio04KanbanService;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.UUID;
+import java.util.stream.Collector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Implementa la interface IEjercicio04KanbanService
  */
+@Service
 public class Ejercicio04KanbanService implements IEjercicio04KanbanService {
+
+    private List<TareaKanban> listaKanbans = new ArrayList<>();
 
     @Override
     public void crearTarea(String descripcion, Integer horasEstimacion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TareaKanban tarea = new TareaKanban();
+
+        tarea.setCodigo(UUID.randomUUID().toString());
+        tarea.setDescripcion(descripcion);
+        tarea.setPropietario(null);
+        tarea.setHorasEstimacion(horasEstimacion);
+        tarea.setHorasTrabajadas(0);
+        tarea.setEstado("Roadmap");
+
+        listaKanbans.add(tarea);
     }
 
+    //*********************************NO PASA EL TEST****************************//
     @Override
     public void modificarTarea(String codigo, String descripcion, Integer horasEstamacion) throws OperacionEnListaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Optional<TareaKanban> tareaOptional = getTarea(codigo);
+
+        if (tareaOptional.isPresent()) {
+            TareaKanban tareaKanban = new TareaKanban();
+            tareaKanban.setDescripcion(descripcion);
+            tareaKanban.setHorasEstimacion(horasEstamacion);
+            
+            listaKanbans.add(tareaKanban);
+        }else{
+            throw new OperacionEnListaException(codigo);
+        }
     }
 
     @Override
@@ -39,12 +66,13 @@ public class Ejercicio04KanbanService implements IEjercicio04KanbanService {
 
     @Override
     public List<TareaKanban> getTareas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return listaKanbans;
     }
 
     @Override
     public Optional<TareaKanban> getTarea(String codigo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return listaKanbans.stream()
+                .filter(x -> x.getCodigo().equals(codigo))
+                .findFirst();
     }
-    
 }
