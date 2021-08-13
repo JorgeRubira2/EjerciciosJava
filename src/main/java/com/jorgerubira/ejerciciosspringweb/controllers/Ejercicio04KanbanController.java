9da0917247fe12233ea0eab05e7bbea7fb916eec
Sofia Rebagliato <sofia.rebagliatto@gmail.com>
2021-08-13 13:33:04 +0200

    El ejercicio 4 por la mitad

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio04KanbanController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio04KanbanController.java
@@ -1,34 +1,98 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
 import com.jorgerubira.ejerciciosspringweb.domain.TareaKanban;
+import com.jorgerubira.ejerciciosspringweb.exceptions.OperacionEnListaException;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio04KanbanService;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-/**
- * Cosas interesantes para implementar en la vista.
- * Si las horas superiores es superior que el doble de la hora estimada marcar el fondo en rojo
- * Si las horas superiores es superior a la hora estimada marcar el fondo en naranja
- * Si las horas superiores es inferior o igual a la hora estimada marcar el fondo en verde
- * 
- * En las tarjetas mostrar la descripción, persona responsable, horas trabajadas/horas estimadas.
- * 
- */
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
+/**
+ * Cosas interesantes para implementar en la vista. Si las horas superiores es
+ * superior que el doble de la hora estimada marcar el fondo en rojo Si las
+ * horas superiores es superior a la hora estimada marcar el fondo en naranja Si
+ * las horas superiores es inferior o igual a la hora estimada marcar el fondo
+ * en verde
+ *
+ * En las tarjetas mostrar la descripción, persona responsable, horas
+ * trabajadas/horas estimadas.
+ *
+ */
 @Controller
 @RequestMapping("/controlador")
 public class Ejercicio04KanbanController {
-    
+
     @Autowired
     private IEjercicio04KanbanService service;
-    
+
     @GetMapping("/kanban")
     public String kanban(Model model) {
-        model.addAttribute("tarea", new TareaKanban());
-        model.addAttribute("tarea", service.getTareas());
-    return "ej04/kanban";
+
+        model.addAttribute("Roadmap", service.getTareas().stream().filter(t -> t.getEstado().equals("Roadmap")).collect(Collectors.toList()));
+        model.addAttribute("Waiting", service.getTareas().stream().filter(t -> t.getEstado().equals("Waiting")).collect(Collectors.toList()));
+        model.addAttribute("Working", service.getTareas().stream().filter(t -> t.getEstado().equals("Working")).collect(Collectors.toList()));
+        model.addAttribute("Done", service.getTareas().stream().filter(t -> t.getEstado().equals("Done")).collect(Collectors.toList()));
+
+        return "ej04/kanban";
+    }
+
+    @GetMapping("/read")
+    @ResponseBody
+    public TareaKanban read(String codigo) {
+        return service.getTarea(codigo).get();
+    }
+
+    @PostMapping("/nueva")
+    public RedirectView nueva(Model model, String descripcion, Integer horasEstimacion) {
+        service.crearTarea(descripcion, horasEstimacion);
+        return new RedirectView("kanban");
+    }
+
+    @PostMapping("/edit")
+    public RedirectView edit(Model model, String codigo, String descripcion, Integer horasEstamacion) {
+        try {
+            service.modificarTarea(codigo, descripcion, horasEstamacion);
+        } catch (OperacionEnListaException e) {
+            Logger.getLogger(Ejercicio04KanbanController.class.getName());
+        }
+        return new RedirectView("kanban");
     }
     
+    @PostMapping("/asignar")
+    public RedirectView asignar(Model model, String codigo, String persona) {
+        try {
+            service.asignarPersona(codigo, persona);
+        } catch (OperacionEnListaException e) {
+            Logger.getLogger(Ejercicio04KanbanController.class.getName());
+        }
+        return new RedirectView("kanban");
+    }
+    
+    @PostMapping("/imputar")
+    public RedirectView imputar(Model model, String codigo, int horas) {
+        try {
+            service.imputarHorasTrabajadas(codigo, horas);
+        } catch (OperacionEnListaException e) {
+            Logger.getLogger(Ejercicio04KanbanController.class.getName());
+        }
+        return new RedirectView("kanban");
+    }
+    
+    @PostMapping("/estado")
+    public RedirectView estado(Model model, String codigo, String persona) {
+        try {
+            service.cambiarEstado(codigo, persona);
+        } catch (OperacionEnListaException e) {
+            Logger.getLogger(Ejercicio04KanbanController.class.getName());
+        }
+        return new RedirectView("kanban");
+    }
+
 }
--- src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio04KanbanService.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio04KanbanService.java
@@ -6,56 +6,116 @@ import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio04KanbanService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
-
 
 /**
  * Implementa la interface IEjercicio04KanbanService
  */
 
 /**
- private String codigo; //Se generará al azar al insertar. UUID.randomUUID().toString();
-    private String descripcion; //Descripcion de la tarea
-    private String propietario; //Persona que está realizando esta tarea.
-    private Integer horasEstimacion; //Tiempo estimado en la tarea. Opcional
-    private Integer horasTrabajadas; //Tiempo de trabajo en la tarea. 
-    private String estado; //Roadmap (Pensada), Waiting (Se hace en breve), Working (Realizandose), Done (Finalizada). 
-    
+ * public class TareaKanban {
+ *
+ * private String codigo; //Se generará al azar al insertar.
+ * UUID.randomUUID().toString(); private String descripcion; //Descripcion de la
+ * tarea private String propietario; //Persona que está realizando esta tarea.
+ * private Integer horasEstimacion; //Tiempo estimado en la tarea. Opcional
+ * private Integer horasTrabajadas; //Tiempo de trabajo en la tarea. private
+ * String estado; //Roadmap (Pensada), Waiting (Se hace en breve), Working
+ * (Realizandose), Done (Finalizada).  *
+ * }
  */
-
 @Service
 public class Ejercicio04KanbanService implements IEjercicio04KanbanService {
 
     private List<TareaKanban> listaTareaKanban = new ArrayList<>();
-    
-    public Ejercicio04KanbanService() {
-        //listaTareaKanban.add(new TareaKanban("Crear formulario", "Mario", horasEstimacion, horasTrabajadas, "Done"));
-        //listaTareaKanban.add(new TareaKanban("Modificar tabla", "Sonia", 10, null, "Waiting"));
-    }
-    
+
     @Override
     public void crearTarea(String descripcion, Integer horasEstimacion) {
-       //listaTareaKanban.add();
+        /**
+         * Inserta una tarea nueva en la lista 
+         * El campo codigo se obtendrá al azar.
+         * El estado por defecto es Roadmap. 
+         * El numero de hora trabajadas por defecto es 0.
+         * La persona por defecto es null.
+         */
+
+        listaTareaKanban.add(new TareaKanban(UUID.randomUUID().toString(), descripcion, null, horasEstimacion, 0, "Roadmap"));
+
     }
 
     @Override
     public void modificarTarea(String codigo, String descripcion, Integer horasEstamacion) throws OperacionEnListaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+         * Modifica la tarea de la misma que tenga el código introducido.
+         * Devuelve una excepción si el código no encuentra la tarea
+         */
+
+        Optional<TareaKanban> modificar = listaTareaKanban.stream()
+                .filter(t -> t.getCodigo().equals(codigo))
+                .findFirst();
+
+        if (modificar.isPresent()) {
+            modificar.get().setDescripcion(descripcion);
+            modificar.get().setHorasEstimacion(horasEstamacion);
+        } else {
+            throw new OperacionEnListaException(codigo);
+        }
+
     }
 
     @Override
     public void asignarPersona(String codigo, String persona) throws OperacionEnListaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+         * Asigna una persona a la tarea del codigo introducido Devuelve una
+         * excepción si el código no encuentra la tarea
+         */
+        Optional<TareaKanban> asignarPers = listaTareaKanban.stream()
+                .filter(t -> t.getCodigo().equals(codigo))
+                .findFirst();
+
+        if (asignarPers.isPresent()) {
+            asignarPers.get().setPropietario(persona);
+        } else {
+            throw new OperacionEnListaException(codigo);
+        }
+
     }
 
     @Override
     public void imputarHorasTrabajadas(String codigo, int horas) throws OperacionEnListaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+         * Incrementa el tiempo trabajado en la tarea del codigo introducido.
+         * Devuelve una excepción si el código no encuentra la tarea
+         */
+        Optional<TareaKanban> imputarHoras = listaTareaKanban.stream()
+                .filter(t -> t.getCodigo().equals(codigo))
+                .findFirst();
+
+        if (imputarHoras.isPresent()) {
+            imputarHoras.get().setHorasTrabajadas(horas);
+        } else {
+            throw new OperacionEnListaException(codigo);
+        }
+
     }
 
     @Override
     public void cambiarEstado(String codigo, String persona) throws OperacionEnListaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+         * Cambiar de estado de la tarea del codigo introducido. Devuelve una
+         * excepción si el código no encuentra la tarea
+         */
+
+        Optional<TareaKanban> estado = listaTareaKanban.stream()
+                .filter(t -> t.getCodigo().equals(codigo))
+                .findFirst();
+
+        if (estado.isPresent()) {
+            estado.get().setEstado(persona);
+        } else {
+            throw new OperacionEnListaException(codigo);
+        }
     }
 
     @Override
@@ -69,5 +129,5 @@ public class Ejercicio04KanbanService implements IEjercicio04KanbanService {
                 .filter(TareaKanban -> codigo == TareaKanban.getCodigo())
                 .findFirst();
     }
-    
+
 }
