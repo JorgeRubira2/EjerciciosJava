59088b7e28bcba96b7b209f10873f69c42ad0d9d
raulf <fmedinaraul@gmail.com>
2021-08-14 16:53:38 +0200

    Prueba InteliJ Idea

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio04KanbanController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio04KanbanController.java
@@ -6,6 +6,13 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
 
+import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio04KanbanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Cosas interesantes para implementar en la vista.
  * Si las horas superiores es superior que el doble de la hora estimada marcar el fondo en rojo
@@ -15,8 +22,19 @@ package com.jorgerubira.ejerciciosspringweb.controllers;
  * En las tarjetas mostrar la descripción, persona responsable, horas trabajadas/horas estimadas.
  * 
  */
+
 @Controller
-@RequestMapping("/ejercicio4")
+@RequestMapping("/Ejercicio4")
 public class Ejercicio04KanbanController {
-    
+
+    @Autowired
+    private IEjercicio04KanbanService list;
+
+    @GetMapping("")
+    public String list(Model model) {
+
+        model.addAttribute("lista", list.getTareas());
+
+        return "ej04/Kanban";
+    }
 }
