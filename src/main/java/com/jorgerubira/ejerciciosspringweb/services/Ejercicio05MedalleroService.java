9fc5470d6a8602a0a4ad2dee43737f327205433c
Mohamad <Mohamad@LAPTOP-MOHA>
2021-08-30 14:41:43 +0200

    update diario

--- src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio05MedalleroService.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio05MedalleroService.java
@@ -6,17 +6,23 @@
 package com.jorgerubira.ejerciciosspringweb.services;
 
 import com.jorgerubira.ejerciciosspringweb.domain.Medalla;
+import com.jorgerubira.ejerciciosspringweb.domain.MedallaAtleta;
+import com.jorgerubira.ejerciciosspringweb.domain.MedallaPais;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
 /**
  *
  */
+@Service
 public class Ejercicio05MedalleroService {
-    
+
     private List<Medalla> medallas = new ArrayList<>();
-    
-    public Ejercicio05MedalleroService(){ 
+
+    public Ejercicio05MedalleroService() {
         medallas.add(new Medalla("China", "Peso medio femenino", "Oro", "Lucha", "Li Qian"));
         medallas.add(new Medalla("Gran Bretaña", "Peso medio femenino", "Plata", "Lucha", "Lauren Price"));
         medallas.add(new Medalla("Kenia", "Maratón masculino", "Oro", "Atletismo", "E. Kipchoge"));
@@ -29,5 +35,61 @@ public class Ejercicio05MedalleroService {
         medallas.add(new Medalla("China", "Clavados masculino", "Plata", "Natacion", "J. Yang"));
         medallas.add(new Medalla("Gran Bretaña", "Clavados masculino", "Cobre", "Natacion", "T. Daley"));
     }
-    
+
+    public void altaMedalla(Medalla medalla) {
+        medallas.add(medalla);
+    }
+
+    public List<Medalla> getMedallas() {
+
+        return medallas;
+    }
+
+    public List<MedallaPais> obtenerRankingPorPais() {
+        List<String> paises = medallas.stream()
+                .map(x -> x.getPais())
+                .distinct()
+                .collect(Collectors.toList());
+        List<MedallaPais> resultado = paises.stream()
+                .map(x -> new MedallaPais(
+                x,
+                (int) medallas.stream().filter(
+                        y -> y.getMedalla() == "Oro" && y.getPais().equals(x)).count(),
+                (int) medallas.stream().filter(
+                        y -> y.getMedalla() == "Plata" && y.getPais().equals(x)).count(),
+                (int) medallas.stream().filter(
+                        y -> y.getMedalla() == "Cobre" && y.getPais().equals(x)).count()
+        ))
+                .collect(Collectors.toList());
+        return resultado;
+    }
+
+    public List<String> obtenerDeportesDeUnaMedalla(String pais, String medalla) {
+        List<String> resultado = medallas.stream()
+                .filter(x -> x.getPais().equals(pais) && x.getMedalla().equals(medalla))
+                .map(x -> x.getDeporte())
+                .distinct()
+                .collect(Collectors.toList());
+        return resultado;
+    }
+
+//    public List<String> obtenerDeportesConMedalla(String pais) {
+//
+//        List<String> deportes = medallas.stream()
+//                .filter(x->x.getPais().equals(pais))
+//                .map(x->x.getDeporte())
+//                .distinct()
+//                .collect(Collectors.toList());
+//        List<String> resultado = deportes.stream()
+//                 .map(x-> medallas.stream().filter(y->y.getPais().equals(pais)&&y.getDeporte().equals(x).map(y->y.getMedalla())))
+//                 .collect(Collectors.toList());
+//        
+//                 
+//                
+//      return resultado;
+//    }
+
+//    public List<MedallaAtleta> obtenerRankingPorAlteta() {
+//
+//    }
 }
--- src/main/java/com/jorgerubira/explicaciones/D20210811/CancionController.java
+++ src/main/java/com/jorgerubira/explicaciones/D20210811/CancionController.java
@@ -25,7 +25,7 @@ public class CancionController {
     
     @GetMapping("/lista")
     public String leerCanciones(Model m){ 
-        m.addAttribute("canciones", repositorio.findByAutorContaining("E"));   
+        m.addAttribute("canciones", repositorio.findAll());   
         return "d20210811/listado";      
     }
     
