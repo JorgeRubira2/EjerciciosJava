50e0b137014920e30d025da31eb7830c433f82bb
Isabel <Isabel@LAPTOP-JF6QL3V5.domain.name>
2021-08-30 14:41:07 +0200

    commit dia 30

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio04KanbanController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio04KanbanController.java
@@ -14,21 +14,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * Cosas interesantes para implementar en la vista.
- * Si las horas superiores es superior que el doble de la hora estimada marcar el fondo en rojo
- * Si las horas superiores es superior a la hora estimada marcar el fondo en naranja
- * Si las horas superiores es inferior o igual a la hora estimada marcar el fondo en verde
- * 
- * En las tarjetas mostrar la descripción, persona responsable, horas trabajadas/horas estimadas.
- * 
+ * Cosas interesantes para implementar en la vista. Si las horas superiores es
+ * superior que el doble de la hora estimada marcar el fondo en rojo Si las
+ * horas superiores es superior a la hora estimada marcar el fondo en naranja Si
+ * las horas superiores es inferior o igual a la hora estimada marcar el fondo
+ * en verde
+ *
+ * En las tarjetas mostrar la descripción, persona responsable, horas
+ * trabajadas/horas estimadas.
+ *
  */
-
 @Controller
 @RestController("/ejercicio04")
 public class Ejercicio04KanbanController {
-    
+
     @Autowired
     private IEjercicio04KanbanService kanbanService;
 
@@ -36,20 +36,21 @@ public class Ejercicio04KanbanController {
     }
 
     @GetMapping("/ver")
-    public String verKanban(Model model){
+    public String verKanban(Model model) {
         model.addAttribute("tareas", kanbanService.getTareas());
-        
+ 
         return "/ej04/kanban";
     }
-    
+
     @PostMapping("/crearTarea")
-    public String crearTarea(Model model, String descripcion, Integer horasEstimadas){
+    public String crearTarea(Model model, String descripcion, Integer horasEstimadas) {
         kanbanService.crearTarea(descripcion, horasEstimadas);
+        
         return "redirect:ver";
     }
-    
+
     @PostMapping("/modificarTarea")
-    public String modificarTarea(Model model, String codigo, String descripcion, Integer horasEstamacion){
+    public String modificarTarea(Model model, String codigo, String descripcion, Integer horasEstamacion) {
         try {
             kanbanService.modificarTarea(codigo, descripcion, horasEstamacion);
         } catch (OperacionEnListaException ex) {
@@ -57,19 +58,19 @@ public class Ejercicio04KanbanController {
         }
         return "redirect:ver";
     }
-    
+
     @PostMapping("/asignarPersona")
-    public String asignarPersona(Model model, String codigo, String persona){
+    public String asignarPersona(Model model, String codigo, String persona) {
         try {
             kanbanService.asignarPersona(codigo, persona);
         } catch (OperacionEnListaException ex) {
             Logger.getLogger(Ejercicio04KanbanController.class.getName()).log(Level.SEVERE, null, ex);
         }
         return "redirect:ver";
-}
-    
+    }
+
     @PostMapping("/horasTrabajadas")
-    public String horasTrabajadas(Model model, String codigo, int horas){
+    public String horasTrabajadas(Model model, String codigo, int horas) {
         try {
             kanbanService.imputarHorasTrabajadas(codigo, horas);
         } catch (OperacionEnListaException ex) {
@@ -77,9 +78,9 @@ public class Ejercicio04KanbanController {
         }
         return "redirect:ver";
     }
-    
+
     @PostMapping("/cambiarEstado")
-    public String cambiaEstado(Model model, String codigo, String estado){
+    public String cambiaEstado(Model model, String codigo, String estado) {
         try {
             kanbanService.cambiarEstado(codigo, estado);
         } catch (OperacionEnListaException ex) {
@@ -87,12 +88,11 @@ public class Ejercicio04KanbanController {
         }
         return "redirect:ver";
     }
-    
+
     @PostMapping("/buscarTarea")
-    public String buscarTarea(Model model, String codigo){
+    public String buscarTarea(Model model, String codigo) {
         model.addAttribute("tarea", kanbanService.getTarea(codigo));
-        
+
         return "/ej03/kanban";
     }
-    
 }
