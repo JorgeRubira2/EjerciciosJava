3572391f463f9e810fc1870cfe21eadbc32cea73
Jochemali <gimenezleon98@gmail.com>
2021-08-30 14:27:05 +0200

    Empezando ejercicio 8, vuelta de vacaciones

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio08MercadoContinuo.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio08MercadoContinuo.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.jorgerubira.ejerciciosspringweb.interfaces.IEjercicio08MercadoContinuoService;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/ejercicio8")
@@ -17,9 +18,10 @@ public class Ejercicio08MercadoContinuo {
     private IEjercicio08MercadoContinuoService mercadoContinuo;
     
     @GetMapping("/listaCotizaciones")
-    @ResponseBody
-    public List<Cotizacion> mercadoContinuo() throws Exception{
-
-        return mercadoContinuo.obtenerCotizaciones();
+    //@ResponseBody
+    //public List<Cotizacion> mercadoContinuo(Model model) throws Exception{
+    public String mercadoContinuo(Model model) throws Exception{
+        model.addAttribute("datosMercado", mercadoContinuo.obtenerCotizaciones());
+        return ("ej08/mercado");
     }
 }
