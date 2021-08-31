2aeb46098ca15e998d8a1bee419327dc05aa7c9e
JorgeRubira <jorgesub@gmail.com>
2021-08-31 10:38:30 +0200

    Merge origin/master into HEAD
    Conflicts:
    	.gitignore
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java

--- src/main/java/com/jorgerubira/explicaciones/D20210830b/IFunciones.java
+++
@@ -1,8 +0,0 @@
-package com.jorgerubira.explicaciones.D20210830;
-
-
-public interface IFunciones {
-    public void funcion1();
-    public void funcion1(int a);
-    public void funcion2(String s);
-}
