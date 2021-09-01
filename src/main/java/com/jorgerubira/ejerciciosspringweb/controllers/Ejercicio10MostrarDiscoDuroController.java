9e3ba19114e348189d8beda989940ead05881c81
Marcos Gimeno <marcosgimeno@gmail.com>
2021-09-01 15:01:41 +0200

    Merge origin/master into marcosg
    Conflicts:
    	src/main/resources/application.properties

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio10MostrarDiscoDuroController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio10MostrarDiscoDuroController.java
@@ -1,9 +1,16 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,4 +36,29 @@ public class Ejercicio10MostrarDiscoDuroController {
         
         return "/ej10/index";
     }
+    
+    @GetMapping("/descarga")
+    public ResponseEntity<Resource>  mostrarFormulario(String ruta){
+        
+        File archivo = new File(ruta);
+                
+        HttpHeaders cabeceras=new HttpHeaders();
+        cabeceras.add("Content-Disposition", "attachment; filename="+archivo.getName());
+        cabeceras.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        cabeceras.add("Pragma", "no-cache");
+        cabeceras.add("Expires", "0");
+        
+        try{
+            return ResponseEntity.ok()
+                                 .headers(cabeceras)
+                                 .contentLength((new File(ruta)).length())
+                                 .contentType(MediaType.parseMediaType( "application/octet-stream" ))  //Codigo MIME
+                                 .body(new InputStreamResource(new FileInputStream( ruta )) );
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+            return null;
+        }
+        
+    }    
+    
 }
