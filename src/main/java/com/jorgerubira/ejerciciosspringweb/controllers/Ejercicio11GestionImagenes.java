063fdaed7dcfffca1fe90e6fc3901266501b21fc
Javier A <javialiaga2000@gmail.com>
2021-09-01 14:51:44 +0200

    Act

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio11GestionImagenes.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio11GestionImagenes.java
@@ -4,6 +4,9 @@ package com.jorgerubira.ejerciciosspringweb.controllers;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,17 +18,52 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/ejercicio10")
-public class Ejercicio10MostrarDiscoDuroController {
+@RequestMapping("/ejercicio11")
+public class Ejercicio11GestionImagenes {
     
     @Value("${carpetas.recursos.hiberus}")
     private String rutaRecursos;
     
+    @GetMapping("/gestor")
+    public String gestion(){
+    
+        return "ej11/gestionimagenes";
+    }
+    
+    @PostMapping("/subir")
+    public String subir(Model m, MultipartFile fichero){ //, HttpServletResponse response){
+        
+        if (fichero.getOriginalFilename().toLowerCase().endsWith(".jpg")==false){
+            m.addAttribute("error", "Formato incorrecto");
+            return "d20210901/formulario";
+        }
+        //UUID.randomUUID().toString();
+        String ruta=rutaRecursos + "\\d20210901\\ejemplo1\\" + fichero.getOriginalFilename();
+        File f=new File(ruta);
+        f.getParentFile().mkdirs();
+        try{
+            Files.copy(fichero.getInputStream(), f.toPath(), StandardCopyOption.REPLACE_EXISTING);    
+            return "redirect:ver?success=Fichero subido";
+        }catch(IOException e){
+            e.printStackTrace();
+            m.addAttribute("error", "Error inesperado");
+             return "redirect:ver";
+        }
+        
+        //response.addCookie(new Cookie("username", "Jovan"));
+       
+    }
+    
+    
+    
+    
     @GetMapping("/ver")
-    public String gestion(Model model, String ruta){
+    public String ver(Model model, String ruta){
         String path="";
         String[] dirFrag = null;
         List<String> ficheros = new ArrayList();
