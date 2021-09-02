970c84d43c683206156d68b86edaa44d70f3ac19
JorgeRubira <jorgesub@gmail.com>
2021-09-02 12:51:41 +0200

    Merge origin/master into HEAD
    Conflicts:
    	.gitignore
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java

---
+++ src/main/java/com/jorgerubira/explicaciones/D20210902/relaciones/Relaciones.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.explicaciones.D20210902.relaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Relaciones {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Relaciones.class, args);
+    }
+}
