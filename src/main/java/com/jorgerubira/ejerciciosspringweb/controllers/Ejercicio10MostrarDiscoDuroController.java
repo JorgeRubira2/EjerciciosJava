818e2cd2d41e4e5a1fc22feb2ef66b9118d95ac7
Christian <Christian@DESKTOP-CSM9Q49>
2021-09-02 16:08:48 +0200

    Merge origin/master into Sofia_F
    Conflicts:
    	src/main/java/com/jorgerubira/ejerciciosspringweb/services/Ejercicio03GestionAlumnosService.java

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio10MostrarDiscoDuroController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio10MostrarDiscoDuroController.java
@@ -2,9 +2,17 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/ejercicio10")
 
 public class Ejercicio10MostrarDiscoDuroController {
-    
-
-     
+         
     @GetMapping("/lista")
     public String listaArchivos(Model model, String ruta){
         List<File> lista = new ArrayList<>();  
@@ -41,10 +47,32 @@ public class Ejercicio10MostrarDiscoDuroController {
          }      
         
         model.addAttribute("lista", lista);         
-        
-        
         return "ej10/mostrarDisco";
     }
 
+    
+    @GetMapping("/descarga")
+    public ResponseEntity<Resource> descarga(Model model, String ruta) throws IOException{
+       
+        File file=new File(ruta);
+        
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Disposition", "attachment; filename=" + file.getName());
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache"); 
+        header.add("Expires", "0"); 
+        
+        //try{
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(ruta));
+            return ResponseEntity.ok()
+                                 .headers(header)
+                                 .contentLength(file.length())
+                                 .contentType(MediaType.parseMediaType("application/octet-stream"))
+                                 .body(resource); 
+        /*}catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }*/
+    }
     
 }
