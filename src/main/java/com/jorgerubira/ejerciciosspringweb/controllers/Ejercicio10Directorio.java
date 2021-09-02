bb02ceb5cc42bf194d40c625ae25204773d46860
Adrian Cachay <acachayep@gmail.com>
2021-09-02 14:48:47 +0200

    cambios sin importancia
    Signed-off-by: Adrian Cachay <acachayep@gmail.com>

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio10Directorio.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio10Directorio.java
@@ -1,16 +1,9 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,27 +42,5 @@ public class Ejercicio10Directorio {
 		}
 		return "ej10/lista";
 	}
-	
-	@GetMapping("/descarga")
-	public ResponseEntity<Resource>  mostrarFormulario(int imagen){
-		String ruta=rutaRecursos + "\\d20210901\\ejemplo1\\imagen" + imagen +".jpg";
-
-		HttpHeaders cabeceras=new HttpHeaders();
-		cabeceras.add("Content-Disposition", "attachment; filename=imagen.jpg");
-		cabeceras.add("Cache-Control", "no-cache, no-store, must-revalidate");
-		cabeceras.add("Pragma", "no-cache");
-		cabeceras.add("Expires", "0");
-
-		try{
-			return ResponseEntity.ok()
-					.headers(cabeceras)
-					.contentLength((new File(ruta)).length())
-					.contentType(MediaType.parseMediaType( "application/octet-stream" ))  //Codigo MIME
-					.body(new InputStreamResource(new FileInputStream( ruta )) );
-		}catch(FileNotFoundException e){
-			e.printStackTrace();
-			return null;
-		}
-        
-    }  
+ 
 }
