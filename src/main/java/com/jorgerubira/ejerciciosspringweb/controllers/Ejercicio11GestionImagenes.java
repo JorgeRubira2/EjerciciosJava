8db0ceccc7174421a9eb3961f94e3f787c85e8a9
Isacoro <cisda@hotmail.com>
2021-09-03 14:03:59 +0200

    Commit final dia 3/9

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio11GestionImagenes.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio11GestionImagenes.java
@@ -1,8 +1,17 @@
 
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import com.jorgerubira.ejerciciosspringweb.entities.Imagen;
+import com.jorgerubira.ejerciciosspringweb.repositories.ImagenRepository;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -11,6 +20,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/ejercicio11")
 public class Ejercicio11GestionImagenes {
+    
+    @Autowired
+    private ImagenRepository imagenRepo;
+    
+    @Value("${carpetas.recursos.hiberus}")
+    private String rutaRecursos;
+    
+    
+    @GetMapping("/ver")
+    public String verFormulario(Model model, String success){
+        model.addAttribute("success", success);
+        return "/ej11/formulario";
+    }
+    
+    //SEGUIR AQUÍ......................................
+    @PostMapping("/subir")
+    public String subirFichero(Model model, MultipartFile fichero){
+        String nombre = UUID.randomUUID().toString();
+        String ruta = rutaRecursos + "\\ejercicio11\\" + nombre;
+        
+        return "ej11/formulario";
+    }
+    
+    
+    
  
 
     
---
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/repositories/ImagenRepository.java
@@ -0,0 +1,17 @@
+
+package com.jorgerubira.ejerciciosspringweb.repositories;
+
+import com.jorgerubira.ejerciciosspringweb.entities.Imagen;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Isabel
+ */
+@Repository
+public interface ImagenRepository extends JpaRepository<Imagen, Long>{
+    
+    List<Imagen> findByDescripcion(String descripcion);
+}
