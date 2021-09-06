422f9a6f6725f457d5af3df58a4b3b6d962fe0e9
Javier A <javialiaga2000@gmail.com>
2021-09-06 14:01:14 +0200

    Act

--- src/main/java/com/jorgerubira/ejerciciosspringweb/repositories/FicheroCSVRepository.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/repositories/FicheroCSVRepository.java
@@ -2,9 +2,10 @@
 package com.jorgerubira.ejerciciosspringweb.repositories;
 
 import com.jorgerubira.ejerciciosspringweb.entities.Cliente;
+import com.jorgerubira.ejerciciosspringweb.entities.FicheroCSV;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface FicheroCSVRepository extends JpaRepository<Cliente, Long>{
+public interface FicheroCSVRepository extends JpaRepository<FicheroCSV, Long>{
     
 }
