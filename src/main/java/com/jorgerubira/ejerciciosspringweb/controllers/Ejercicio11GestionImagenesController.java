3b28c8d2ab59e8dc635be8adbb69f81ed3ab665c
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-09-07 16:25:41 +0200

    ej11 casi terminado , falta filtro

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio11GestionImagenesController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio11GestionImagenesController.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
@@ -23,22 +24,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.jorgerubira.ejerciciosspringweb.entities.Imagen;
-import com.jorgerubira.ejerciciosspringweb.repositories.ImagenRepository;
+
+import com.jorgerubira.ejerciciosspringweb.repositories.imagenesRepository;
 
 @Controller
 @RequestMapping("/ejercicio11")
 public class Ejercicio11GestionImagenesController {
 	
-	@Autowired(required = false) //no es obligatorio el repository
-	private ImagenRepository imagenRepository ;
+	@Autowired(required = false) 
+	private imagenesRepository imagenRepository ;
 	
 	@Value("${carpetas.recursos.hiberus}")
 	private String rutaRecursos;
 	
 	@GetMapping("/vista")
-    public String vista(Model m) {
+    public String vista(Model model) {
 
-        m.addAttribute("imagenes", imagenRepository.findAll());
+        model.addAttribute("imagenes", imagenRepository.findAll());
 
         return "ej11/vista";
 
@@ -50,7 +52,7 @@ public class Ejercicio11GestionImagenesController {
 		
 		File file = new File(ruta);
 		file.getParentFile().mkdirs();
-		
+		//guardar el archivo
 		try {
 			Files.copy(fichero.getInputStream(),file.toPath(),StandardCopyOption.REPLACE_EXISTING);
 			
@@ -68,7 +70,7 @@ public class Ejercicio11GestionImagenesController {
 	}
 	@GetMapping("/descargar")
     public ResponseEntity<Resource> descarga(String nombre) {
-		String ruta = rutaRecursos + "\\ejericio11\\imagenes\\" + nombre;
+		String ruta = rutaRecursos + "\\ejercicio11\\imagenes\\" + nombre;
         
         HttpHeaders cabeceras = new HttpHeaders();
         cabeceras.add("Content-Disposition", "attachment; filename=" + nombre);
@@ -87,6 +89,19 @@ public class Ejercicio11GestionImagenesController {
             return null;
         }
 
+    }
+	@GetMapping("/borrar")
+    public String borrar(Integer id){
+        String nombre=imagenRepository.findById(id).get().getNombre();
+        String ruta = rutaRecursos + "\\ejercicio11\\imagenes\\" + nombre;
+        imagenRepository.deleteById(id);
+        try {
+            Files.deleteIfExists(Paths.get(ruta));
+        } catch (IOException ex) {
+           
+        }
+    
+        return "redirect:vista";
     }
 	
 
--- src/main/java/com/jorgerubira/ejerciciosspringweb/repositories/imagenesRepository.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/repositories/imagenesRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jorgerubira.ejerciciosspringweb.entities.Imagen;
 
-public interface ImagenRepository extends JpaRepository<Imagen, Long>{
-
-    public Imagen findByCodigo(String codigo,String descripcion);
+public interface imagenesRepository extends JpaRepository<Imagen, Integer> {
     
 }
