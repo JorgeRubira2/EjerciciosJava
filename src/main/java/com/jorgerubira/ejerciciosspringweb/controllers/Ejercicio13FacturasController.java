0c1518ab046eaaea3d34d2b61a7ff382f4cf17b7
Adrian Cachay <acachayep@gmail.com>
2021-09-08 14:56:48 +0200

    Empiezo Ejercicio13
    Signed-off-by: Adrian Cachay <acachayep@gmail.com>

--- src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio13FacturasController.java
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/controllers/Ejercicio13FacturasController.java
@@ -1,10 +1,18 @@
 package com.jorgerubira.ejerciciosspringweb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jorgerubira.ejerciciosspringweb.repositories.FacturaRepository;
+
+import lombok.Setter;
 
 @Controller
 @RequestMapping("/ejercicio13")
 public class Ejercicio13FacturasController {
 
+    @Setter 
+    @Autowired(required = false)    //Con required = false no es obligatorio que esté el repository
+    private FacturaRepository facturasRepo;
 }
---
+++ src/main/java/com/jorgerubira/ejerciciosspringweb/repositories/FacturaRepository.java
@@ -0,0 +1,9 @@
+package com.jorgerubira.ejerciciosspringweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jorgerubira.ejerciciosspringweb.entities.Factura;
+
+public interface FacturaRepository extends JpaRepository<Factura, Integer>{
+
+}
